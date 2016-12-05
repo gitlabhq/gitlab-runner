hu19891110/gitlab-ci-multi-runner

Add pprof http endpoints to metrics server

commands/multi.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net"
 	"net/http"
+	_ "net/http/pprof" // PPROF package adds everyting itself inside its init() function
 	"os"
 	"os/signal"
 	"runtime"

gitlabhq/gitlab-runner

commands/multi.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net"
 	"net/http"
+	_ "net/http/pprof" // PPROF package adds everything itself inside its init() function
 	"os"
 	"os/signal"
 	"runtime"