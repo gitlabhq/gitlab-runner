c61bb9be713e12ecc551087f1fd7bdc4c7f20cf2
Birger Schmidt <bs-github@netgaroo.com>
2017-02-19 20:22:16 +0100

    better tests for timeperiod functions

--- helpers/timeperiod/period_test.go
+++ helpers/timeperiod/period_test.go
@@ -18,23 +18,50 @@ var daysOfWeek = map[time.Weekday]string{
 	time.Sunday:    "sun",
 }
 
-func newTimePeriods(t *testing.T) (time.Time, *TimePeriod) {
-	now := time.Now()
+func newTimePeriods(t *testing.T, timezone string) (time.Time, *TimePeriod) {
+	location, err := time.LoadLocation(timezone)
+
+	now := time.Now().In(location)
 	minute := now.Minute()
 	hour := now.Hour()
 	day := now.Weekday()
 	dayName := daysOfWeek[day]
 
 	periodPattern := fmt.Sprintf("* %d %d * * %s *", minute, hour, dayName)
-	timezone := "Europe/Warsaw"
 	timePeriods, err := TimePeriods([]string{periodPattern}, timezone)
+	fmt.Printf("newTimePeriods: %s\n", []string{periodPattern})
+
 	assert.NoError(t, err)
 
 	return now, timePeriods
 }
 
+func TestWrongTimezone(t *testing.T) {
+	assert.Panics(t, func() {
+		_, _ = newTimePeriods(t, "NoValidTimezone/String")
+	}, "calling newTimePeriods with a wrong timezone string should panic")
+}
+
+func TestTimezone(t *testing.T) {
+	localtimezone, _ := time.Now().Zone()
+	_, timePeriods := newTimePeriods(t, localtimezone)
+
+	fmt.Printf("TestLocalTimezone: %s - %s\n", timePeriods.GetCurrentTime(), time.Now())
+	assert.WithinDuration(t, timePeriods.GetCurrentTime(), time.Now(), 1*time.Second, "The difference should not be more than 1s")
+
+	_, timePeriods = newTimePeriods(t, "America/New_York")
+	fmt.Printf("TestNewYorkTimezone: %s - %s\n", timePeriods.GetCurrentTime(), time.Now())
+	assert.WithinDuration(t, timePeriods.GetCurrentTime(), time.Now(), 1*time.Second, "The difference should not be more than 1s")
+
+	_, timePeriods = newTimePeriods(t, "Australia/Sydney")
+	location, _ := time.LoadLocation("Australia/Sydney")
+	fmt.Printf("TestSydneyTimezone: %s - %s\n", timePeriods.GetCurrentTime(), time.Now())
+	assert.WithinDuration(t, timePeriods.GetCurrentTime(), time.Now(), 1*time.Second, "The difference should not be more than 1s")
+	assert.Equal(t, timePeriods.GetCurrentTime().Format("3:04PM"), time.Now().In(location).Format("3:04PM"))
+}
+
 func TestInPeriod(t *testing.T) {
-	now, timePeriods := newTimePeriods(t)
+	now, timePeriods := newTimePeriods(t, "")
 	timePeriods.GetCurrentTime = func() time.Time {
 		return now
 	}
@@ -43,20 +70,23 @@ func TestInPeriod(t *testing.T) {
 }
 
 func TestPeriodOut(t *testing.T) {
-	now, timePeriods := newTimePeriods(t)
+	now, timePeriods := newTimePeriods(t, "")
 	timePeriods.GetCurrentTime = func() time.Time {
+		fmt.Printf("now.Add(time.Hour * 48): %+v\n", now.Add(time.Hour*48))
 		return now.Add(time.Hour * 48)
 	}
 	assert.False(t, timePeriods.InPeriod())
 
-	now, timePeriods = newTimePeriods(t)
+	now, timePeriods = newTimePeriods(t, "")
 	timePeriods.GetCurrentTime = func() time.Time {
+		fmt.Printf("now.Add(time.Hour * 4): %+v\n", now.Add(time.Hour*4))
 		return now.Add(time.Hour * 4)
 	}
 	assert.False(t, timePeriods.InPeriod())
 
-	now, timePeriods = newTimePeriods(t)
+	now, timePeriods = newTimePeriods(t, "")
 	timePeriods.GetCurrentTime = func() time.Time {
+		fmt.Printf("now.Add(time.Minute * 4): %+v\n", now.Add(time.Minute*4))
 		return now.Add(time.Minute * 4)
 	}
 	assert.False(t, timePeriods.InPeriod())
