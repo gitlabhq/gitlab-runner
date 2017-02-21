d2fa7a5128d410f958b88dd760da4739ee8a07aa
Tomasz Maczukin <tomasz@maczukin.pl>
2017-02-22 00:00:51 +0100

    Refactor timeperiod tests

--- helpers/timeperiod/period_test.go
+++ helpers/timeperiod/period_test.go
@@ -31,42 +31,6 @@ func newTimePeriods(t *testing.T) (time.Time, *TimePeriod) {
 	return now, timePeriods
 }
 
-func TestInvalidTimezone(t *testing.T) {
-	_, err := TimePeriods([]string{}, "InvalidTimezone/String")
-	assert.Error(t, err)
-}
-
-func testTimeperiodsWithTimezone(t *testing.T, period, timezone string, month time.Month, day, hour, minute int, inPeriod bool) {
-	timePeriods, _ := TimePeriods([]string{period}, timezone)
-	timePeriods.GetCurrentTime = func() time.Time {
-		return time.Date(2017, month, day, hour, minute, 0, 0, time.UTC)
-	}
-
-	now := timePeriods.GetCurrentTime()
-	nowInLocation := now.In(timePeriods.location)
-	t.Log(fmt.Sprintf("Checking timeperiod '%s' in timezone '%s' for %s (%s)", period, timezone, now, nowInLocation))
-
-	if inPeriod {
-		assert.True(t, timePeriods.InPeriod(), "It should be inside of the period")
-	} else {
-		assert.False(t, timePeriods.InPeriod(), "It should be outside of the period")
-	}
-}
-
-func TestOffPeakPeriod(t *testing.T) {
-	// inside or outside of the timeperiod, basing on DST status
-	testTimeperiodsWithTimezone(t, "* * 10-17 * * * *", "Europe/Berlin", time.January, 1, 16, 30, true)
-	testTimeperiodsWithTimezone(t, "* * 10-17 * * * *", "Europe/Berlin", time.July, 1, 16, 30, false)
-
-	// always inside of the timeperiod
-	testTimeperiodsWithTimezone(t, "* * 10-17 * * * *", "Europe/Berlin", time.January, 1, 14, 30, true)
-	testTimeperiodsWithTimezone(t, "* * 10-17 * * * *", "Europe/Berlin", time.July, 1, 14, 30, true)
-
-	// always outside of the timeperiod
-	testTimeperiodsWithTimezone(t, "* * 10-17 * * * *", "Europe/Berlin", time.January, 1, 20, 30, false)
-	testTimeperiodsWithTimezone(t, "* * 10-17 * * * *", "Europe/Berlin", time.July, 1, 20, 30, false)
-}
-
 func TestInPeriod(t *testing.T) {
 	now, timePeriods := newTimePeriods(t)
 	timePeriods.GetCurrentTime = func() time.Time {
@@ -93,4 +57,43 @@ func TestPeriodOut(t *testing.T) {
 		return now.Add(time.Minute * 4)
 	}
 	assert.False(t, timePeriods.InPeriod())
+}
+
+func TestInvalidTimezone(t *testing.T) {
+	_, err := TimePeriods([]string{}, "InvalidTimezone/String")
+	assert.Error(t, err)
+}
+
+func testTimeperiodsWithTimezone(t *testing.T, period, timezone string, month time.Month, day, hour, minute int, inPeriod bool) {
+	timePeriods, _ := TimePeriods([]string{period}, timezone)
+	timePeriods.GetCurrentTime = func() time.Time {
+		return time.Date(2017, month, day, hour, minute, 0, 0, time.UTC)
+	}
+
+	now := timePeriods.GetCurrentTime()
+	nowInLocation := now.In(timePeriods.location)
+	t.Log(fmt.Sprintf("Checking timeperiod '%s' in timezone '%s' for %s (%s)", period, timezone, now, nowInLocation))
+
+	if inPeriod {
+		assert.True(t, timePeriods.InPeriod(), "It should be inside of the period")
+	} else {
+		assert.False(t, timePeriods.InPeriod(), "It should be outside of the period")
+	}
+}
+
+func TestTimeperiodsWithTimezone(t *testing.T) {
+	period := "* * 10-17 * * * *"
+	timezone := "Europe/Berlin"
+
+	// inside or outside of the timeperiod, basing on DST status
+	testTimeperiodsWithTimezone(t, period, timezone, time.January, 1, 16, 30, true)
+	testTimeperiodsWithTimezone(t, period, timezone, time.July, 1, 16, 30, false)
+
+	// always inside of the timeperiod
+	testTimeperiodsWithTimezone(t, period, timezone, time.January, 1, 14, 30, true)
+	testTimeperiodsWithTimezone(t, period, timezone, time.July, 1, 14, 30, true)
+
+	// always outside of the timeperiod
+	testTimeperiodsWithTimezone(t, period, timezone, time.January, 1, 20, 30, false)
+	testTimeperiodsWithTimezone(t, period, timezone, time.July, 1, 20, 30, false)
 }
