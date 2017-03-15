fcc2cdeb823e6fad164f0c554ecd1f52661eed2c
Kamil Trzcinski <ayufan@ayufan.eu>
2017-03-15 23:39:47 +0100

    Limit number of concurrent requests to builds/register.json

--- commands/builds_helper_test.go
+++ commands/builds_helper_test.go
@@ -6,6 +6,7 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/stretchr/testify/assert"
 
+	"github.com/stretchr/testify/require"
 	"gitlab.com/gitlab-org/gitlab-ci-multi-runner/common"
 )
 
@@ -18,4 +19,86 @@ func TestBuildsHelperCollect(t *testing.T) {
 	})
 	b.Collect(ch)
 	assert.Len(t, ch, 1)
+}
+
+func TestBuildsHelperAcquireRequestWithLimit(t *testing.T) {
+	runner := common.RunnerConfig{
+		RequestConcurrency: 2,
+	}
+
+	b := &buildsHelper{}
+	result := b.acquireRequest(&runner)
+	require.True(t, result)
+
+	result = b.acquireRequest(&runner)
+	require.True(t, result)
+
+	result = b.acquireRequest(&runner)
+	require.False(t, result, "allow only two requests")
+
+	result = b.releaseRequest(&runner)
+	require.True(t, result)
+
+	result = b.releaseRequest(&runner)
+	require.True(t, result)
+
+	result = b.releaseRequest(&runner)
+	require.False(t, result, "release only two requests")
+}
+
+func TestBuildsHelperAcquireRequestWithDefault(t *testing.T) {
+	runner := common.RunnerConfig{
+		RequestConcurrency: 0,
+	}
+
+	b := &buildsHelper{}
+	result := b.acquireRequest(&runner)
+	require.True(t, result)
+
+	result = b.acquireRequest(&runner)
+	require.False(t, result, "allow only one request")
+
+	result = b.releaseRequest(&runner)
+	require.True(t, result)
+
+	result = b.releaseRequest(&runner)
+	require.False(t, result, "release only one request")
+}
+
+func TestBuildsHelperAcquireBuildWithLimit(t *testing.T) {
+	runner := common.RunnerConfig{
+		Limit: 1,
+	}
+
+	b := &buildsHelper{}
+	result := b.acquireBuild(&runner)
+	require.True(t, result)
+
+	result = b.acquireBuild(&runner)
+	require.False(t, result, "allow only one build")
+
+	result = b.releaseBuild(&runner)
+	require.True(t, result)
+
+	result = b.releaseBuild(&runner)
+	require.False(t, result, "release only one build")
+}
+
+func TestBuildsHelperAcquireBuildUnlimited(t *testing.T) {
+	runner := common.RunnerConfig{
+		Limit: 0,
+	}
+
+	b := &buildsHelper{}
+	result := b.acquireBuild(&runner)
+	require.True(t, result)
+
+	result = b.acquireBuild(&runner)
+	require.True(t, result)
+
+	result = b.releaseBuild(&runner)
+	require.True(t, result)
+
+	result = b.releaseBuild(&runner)
+	require.True(t, result)
 }
