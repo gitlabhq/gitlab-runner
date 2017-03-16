302054b405a4b6d10ac57a3f5350a59dc459d971
Tomasz Maczukin <tomasz@maczukin.pl>
2017-03-16 21:07:41 +0100

    Refactorize exec's gitlab-ci.yaml parser

--- commands/exec.go
+++ commands/exec.go
@@ -1,9 +1,6 @@
 package commands
 
 import (
-	"errors"
-	"fmt"
-	"io/ioutil"
 	"os"
 	"os/exec"
 	"strings"
@@ -12,7 +9,7 @@ import (
 	"github.com/codegangsta/cli"
 	"gitlab.com/ayufan/golang-cli-helpers"
 	"gitlab.com/gitlab-org/gitlab-ci-multi-runner/common"
-	"gopkg.in/yaml.v2"
+	"gitlab.com/gitlab-org/gitlab-ci-multi-runner/helpers/gitlab_ci_yaml_parser"
 
 	// Force to load all executors, executes init() on them
 	_ "gitlab.com/gitlab-org/gitlab-ci-multi-runner/executors/docker"
@@ -34,265 +31,6 @@ func (c *ExecCommand) runCommand(name string, arg ...string) (string, error) {
 	cmd.Stderr = os.Stderr
 	result, err := cmd.Output()
 	return string(result), err
-}
-
-func (c *ExecCommand) getCommands(commands interface{}) (common.JRStepScript, error) {
-	if lines, ok := commands.([]interface{}); ok {
-		text := ""
-		for _, line := range lines {
-			if lineText, ok := line.(string); ok {
-				text += lineText + "\n"
-			} else {
-				return common.JRStepScript{}, errors.New("unsupported script")
-			}
-		}
-		return common.JRStepScript(strings.Split(text, "\n")), nil
-	} else if text, ok := commands.(string); ok {
-		return common.JRStepScript(strings.Split(text, "\n")), nil
-	} else if commands != nil {
-		return common.JRStepScript{}, errors.New("unsupported script")
-	}
-	return common.JRStepScript{}, nil
-}
-
-func (c *ExecCommand) buildSteps(config, jobConfig common.BuildOptions) (steps common.JRSteps, err error) {
-	if jobConfig["script"] == nil {
-		err = fmt.Errorf("missing 'script' for job")
-		return
-	}
-
-	var scriptCommands, afterScriptCommands common.JRStepScript
-
-	// get before_script
-	beforeScript, err := c.getCommands(config["before_script"])
-	if err != nil {
-		return
-	}
-
-	// get job before_script
-	jobBeforeScript, err := c.getCommands(jobConfig["before_script"])
-	if err != nil {
-		return
-	}
-
-	if len(jobBeforeScript) < 1 {
-		scriptCommands = beforeScript
-	} else {
-		scriptCommands = jobBeforeScript
-	}
-
-	// get script
-	script, err := c.getCommands(jobConfig["script"])
-	if err != nil {
-		return
-	}
-	for _, scriptLine := range script {
-		scriptCommands = append(scriptCommands, scriptLine)
-	}
-
-	afterScriptCommands, err = c.getCommands(jobConfig["after_script"])
-	if err != nil {
-		return
-	}
-
-	steps = common.JRSteps{
-		common.JRStep{
-			Name:         "script",
-			Script:       scriptCommands,
-			Timeout:      3600,
-			When:         common.StepWhenOnSuccess,
-			AllowFailure: false,
-		},
-		common.JRStep{
-			Name:         "after_script",
-			Script:       afterScriptCommands,
-			Timeout:      3600,
-			When:         common.StepWhenAlways,
-			AllowFailure: false,
-		},
-	}
-
-	return
-}
-
-func (c *ExecCommand) buildVariables(configVariables interface{}) (buildVariables common.BuildVariables, err error) {
-	if variables, ok := configVariables.(map[string]interface{}); ok {
-		for key, value := range variables {
-			if valueText, ok := value.(string); ok {
-				buildVariables = append(buildVariables, common.BuildVariable{
-					Key:    key,
-					Value:  valueText,
-					Public: true,
-				})
-			} else {
-				err = fmt.Errorf("invalid value for variable %q", key)
-			}
-		}
-	} else if configVariables != nil {
-		err = errors.New("unsupported variables")
-	}
-	return
-}
-
-func (c *ExecCommand) buildGlobalAndJobVariables(global, job interface{}) (buildVariables common.BuildVariables, err error) {
-	buildVariables, err = c.buildVariables(global)
-	if err != nil {
-		return
-	}
-
-	jobVariables, err := c.buildVariables(job)
-	if err != nil {
-		return
-	}
-
-	buildVariables = append(buildVariables, jobVariables...)
-	return
-}
-
-func (c *ExecCommand) parseYaml(job string, build *common.JobResponse) error {
-	data, err := ioutil.ReadFile(".gitlab-ci.yml")
-	if err != nil {
-		return err
-	}
-
-	build.JobInfo.Name = job
-
-	// parse gitlab-ci.yml
-	config := make(common.BuildOptions)
-	err = yaml.Unmarshal(data, config)
-	if err != nil {
-		return err
-	}
-
-	err = config.Sanitize()
-	if err != nil {
-		return err
-	}
-
-	// get job
-	jobConfig, ok := config.GetSubOptions(job)
-	if !ok {
-		return fmt.Errorf("no job named %q", job)
-	}
-
-	build.Steps, err = c.buildSteps(config, jobConfig)
-	if err != nil {
-		return err
-	}
-
-	build.Variables, err = c.buildGlobalAndJobVariables(config["variables"], jobConfig["variables"])
-	if err != nil {
-		return err
-	}
-
-	if imageName, ok := getOption("image", config, jobConfig); ok {
-		build.Image.Name = imageName.(string)
-	}
-
-	if services, ok := getOptions("services", config, jobConfig); ok {
-		for _, service := range services {
-			build.Services = append(build.Services, common.JRImage{
-				Name: service.(string),
-			})
-		}
-	}
-
-	artifacts := getOptionsMap("artifacts", config, jobConfig)
-
-	artifactsPaths, _ := artifacts.GetSlice("paths")
-	paths := common.JRArtifactPaths{}
-	for _, path := range artifactsPaths {
-		paths = append(paths, path.(string))
-	}
-
-	var artifactsName string
-	if artifactsName, ok = artifacts.GetString("name"); !ok {
-		artifactsName = ""
-	}
-
-	var artifactsUntracked interface{}
-	if artifactsUntracked, ok = artifacts.Get("untracked"); !ok {
-		artifactsUntracked = false
-	}
-
-	var artifactsWhen string
-	if artifactsWhen, ok = artifacts.GetString("when"); !ok {
-		artifactsWhen = common.ArtifactWhenOnSuccess
-	}
-
-	var artifactsExpireIn string
-	if artifactsExpireIn, ok = artifacts.GetString("expireIn"); !ok {
-		artifactsExpireIn = ""
-	}
-
-	build.Artifacts = make(common.JRArtifacts, 1)
-	build.Artifacts[0] = common.JRArtifact{
-		Name:      artifactsName,
-		Untracked: artifactsUntracked.(bool),
-		Paths:     paths,
-		When:      common.JRArtifactWhen(artifactsWhen),
-		ExpireIn:  artifactsExpireIn,
-	}
-
-	cache := getOptionsMap("cache", config, jobConfig)
-
-	cachePaths, _ := cache.GetSlice("paths")
-	paths = common.JRArtifactPaths{}
-	for _, path := range cachePaths {
-		paths = append(paths, path.(string))
-	}
-
-	var cacheKey string
-	if cacheKey, ok = cache.GetString("key"); !ok {
-		cacheKey = ""
-	}
-
-	var cacheUntracked interface{}
-	if cacheUntracked, ok = cache.Get("untracked"); !ok {
-		cacheUntracked = false
-	}
-
-	build.Cache = make(common.JRCaches, 1)
-	build.Cache[0] = common.JRCache{
-		Key:       cacheKey,
-		Untracked: cacheUntracked.(bool),
-		Paths:     paths,
-	}
-
-	if stage, ok := jobConfig.GetString("stage"); ok {
-		build.JobInfo.Stage = stage
-	} else {
-		build.JobInfo.Stage = "test"
-	}
-
-	return nil
-}
-
-func getOptionsMap(optionKey string, config, jobConfig common.BuildOptions) (value common.BuildOptions) {
-	value, ok := jobConfig.GetSubOptions(optionKey)
-	if !ok {
-		value, _ = config.GetSubOptions(optionKey)
-	}
-
-	return
-}
-
-func getOptions(optionKey string, config, jobConfig common.BuildOptions) (value []interface{}, ok bool) {
-	value, ok = jobConfig.GetSlice(optionKey)
-	if !ok {
-		value, ok = config.GetSlice(optionKey)
-	}
-
-	return
-}
-
-func getOption(optionKey string, config, jobConfig common.BuildOptions) (value interface{}, ok bool) {
-	value, ok = jobConfig.Get(optionKey)
-	if !ok {
-		value, ok = config.Get(optionKey)
-	}
-
-	return
 }
 
 func (c *ExecCommand) createBuild(repoURL string, abortSignal chan os.Signal) (build *common.Build, err error) {
@@ -390,7 +128,8 @@ func (c *ExecCommand) Execute(context *cli.Context) {
 		logrus.Fatalln(err)
 	}
 
-	err = c.parseYaml(c.Job, &build.JobResponse)
+	parser := gitlab_ci_yaml_parser.NewGitLabCiYamlParser(c.Job)
+	err = parser.ParseYaml(&build.JobResponse)
 	if err != nil {
 		logrus.Fatalln(err)
 	}
---
+++ helpers/gitlab_ci_yaml_parser/parser.go
@@ -0,0 +1,318 @@
+package gitlab_ci_yaml_parser
+
+import (
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"strings"
+
+	"gitlab.com/gitlab-org/gitlab-ci-multi-runner/common"
+
+	"gopkg.in/yaml.v2"
+)
+
+type GitLabCiYamlParser struct {
+	jobName   string
+	config    DataBag
+	jobConfig DataBag
+}
+
+func (c *GitLabCiYamlParser) parseFile() (err error) {
+	data, err := ioutil.ReadFile(".gitlab-ci.yml")
+	if err != nil {
+		return err
+	}
+
+	config := make(DataBag)
+	err = yaml.Unmarshal(data, config)
+	if err != nil {
+		return err
+	}
+
+	err = config.Sanitize()
+	if err != nil {
+		return err
+	}
+
+	c.config = config
+
+	return
+}
+
+func (c *GitLabCiYamlParser) loadJob() (err error) {
+	jobConfig, ok := c.config.GetSubOptions(c.jobName)
+	if !ok {
+		return fmt.Errorf("no job named %q", c.jobName)
+	}
+
+	c.jobConfig = jobConfig
+
+	return
+}
+
+func (c *GitLabCiYamlParser) prepareJobInfo(job *common.JobResponse) (err error) {
+	job.JobInfo = common.JRJobInfo{
+		Name: c.jobName,
+	}
+
+	if stage, ok := c.jobConfig.GetString("stage"); ok {
+		job.JobInfo.Stage = stage
+	} else {
+		job.JobInfo.Stage = "test"
+	}
+
+	return
+}
+
+func (c *GitLabCiYamlParser) getCommands(commands interface{}) (common.JRStepScript, error) {
+	if lines, ok := commands.([]interface{}); ok {
+		text := ""
+		for _, line := range lines {
+			if lineText, ok := line.(string); ok {
+				text += lineText + "\n"
+			} else {
+				return common.JRStepScript{}, errors.New("unsupported script")
+			}
+		}
+		return common.JRStepScript(strings.Split(text, "\n")), nil
+	} else if text, ok := commands.(string); ok {
+		return common.JRStepScript(strings.Split(text, "\n")), nil
+	} else if commands != nil {
+		return common.JRStepScript{}, errors.New("unsupported script")
+	}
+
+	return common.JRStepScript{}, nil
+}
+
+func (c *GitLabCiYamlParser) prepareSteps(job *common.JobResponse) (err error) {
+	if c.jobConfig["script"] == nil {
+		err = fmt.Errorf("missing 'script' for job")
+		return
+	}
+
+	var scriptCommands, afterScriptCommands common.JRStepScript
+
+	// get before_script
+	beforeScript, err := c.getCommands(c.config["before_script"])
+	if err != nil {
+		return
+	}
+
+	// get job before_script
+	jobBeforeScript, err := c.getCommands(c.jobConfig["before_script"])
+	if err != nil {
+		return
+	}
+
+	if len(jobBeforeScript) < 1 {
+		scriptCommands = beforeScript
+	} else {
+		scriptCommands = jobBeforeScript
+	}
+
+	// get script
+	script, err := c.getCommands(c.jobConfig["script"])
+	if err != nil {
+		return
+	}
+	for _, scriptLine := range script {
+		scriptCommands = append(scriptCommands, scriptLine)
+	}
+
+	afterScriptCommands, err = c.getCommands(c.jobConfig["after_script"])
+	if err != nil {
+		return
+	}
+
+	job.Steps = common.JRSteps{
+		common.JRStep{
+			Name:         "script",
+			Script:       scriptCommands,
+			Timeout:      3600,
+			When:         common.StepWhenOnSuccess,
+			AllowFailure: false,
+		},
+		common.JRStep{
+			Name:         "after_script",
+			Script:       afterScriptCommands,
+			Timeout:      3600,
+			When:         common.StepWhenAlways,
+			AllowFailure: false,
+		},
+	}
+
+	return
+}
+
+func (c *GitLabCiYamlParser) buildVariables(configVariables interface{}) (buildVariables common.BuildVariables, err error) {
+	if variables, ok := configVariables.(map[string]interface{}); ok {
+		for key, value := range variables {
+			if valueText, ok := value.(string); ok {
+				buildVariables = append(buildVariables, common.BuildVariable{
+					Key:    key,
+					Value:  valueText,
+					Public: true,
+				})
+			} else {
+				err = fmt.Errorf("invalid value for variable %q", key)
+			}
+		}
+	} else if configVariables != nil {
+		err = errors.New("unsupported variables")
+	}
+
+	return
+}
+
+func (c *GitLabCiYamlParser) prepareVariables(job *common.JobResponse) (err error) {
+	job.Variables = common.BuildVariables{}
+	job.Variables, err = c.buildVariables(c.config["variables"])
+	if err != nil {
+		return
+	}
+
+	jobVariables, err := c.buildVariables(c.jobConfig["variables"])
+	if err != nil {
+		return
+	}
+
+	job.Variables = append(job.Variables, jobVariables...)
+
+	return
+}
+
+func (c *GitLabCiYamlParser) prepareImage(job *common.JobResponse) (err error) {
+	job.Image = common.JRImage{}
+	if imageName, ok := getOption("image", c.config, c.jobConfig); ok {
+		job.Image.Name = imageName.(string)
+	}
+
+	return
+}
+
+func (c *GitLabCiYamlParser) prepareServices(job *common.JobResponse) (err error) {
+	job.Services = common.JRServices{}
+
+	if servicesMap, ok := getOptions("services", c.config, c.jobConfig); ok {
+		for _, service := range servicesMap {
+			job.Services = append(job.Services, common.JRImage{
+				Name: service.(string),
+			})
+		}
+	}
+
+	return
+}
+
+func (c *GitLabCiYamlParser) prepareArtifacts(job *common.JobResponse) (err error) {
+	var ok bool
+
+	artifactsMap := getOptionsMap("artifacts", c.config, c.jobConfig)
+
+	artifactsPaths, _ := artifactsMap.GetSlice("paths")
+	paths := common.JRArtifactPaths{}
+	for _, path := range artifactsPaths {
+		paths = append(paths, path.(string))
+	}
+
+	var artifactsName string
+	if artifactsName, ok = artifactsMap.GetString("name"); !ok {
+		artifactsName = ""
+	}
+
+	var artifactsUntracked interface{}
+	if artifactsUntracked, ok = artifactsMap.Get("untracked"); !ok {
+		artifactsUntracked = false
+	}
+
+	var artifactsWhen string
+	if artifactsWhen, ok = artifactsMap.GetString("when"); !ok {
+		artifactsWhen = common.ArtifactWhenOnSuccess
+	}
+
+	var artifactsExpireIn string
+	if artifactsExpireIn, ok = artifactsMap.GetString("expireIn"); !ok {
+		artifactsExpireIn = ""
+	}
+
+	job.Artifacts = make(common.JRArtifacts, 1)
+	job.Artifacts[0] = common.JRArtifact{
+		Name:      artifactsName,
+		Untracked: artifactsUntracked.(bool),
+		Paths:     paths,
+		When:      common.JRArtifactWhen(artifactsWhen),
+		ExpireIn:  artifactsExpireIn,
+	}
+
+	return
+}
+
+func (c *GitLabCiYamlParser) prepareCache(job *common.JobResponse) (err error) {
+	var ok bool
+
+	cacheMap := getOptionsMap("cache", c.config, c.jobConfig)
+
+	cachePaths, _ := cacheMap.GetSlice("paths")
+	paths := common.JRArtifactPaths{}
+	for _, path := range cachePaths {
+		paths = append(paths, path.(string))
+	}
+
+	var cacheKey string
+	if cacheKey, ok = cacheMap.GetString("key"); !ok {
+		cacheKey = ""
+	}
+
+	var cacheUntracked interface{}
+	if cacheUntracked, ok = cacheMap.Get("untracked"); !ok {
+		cacheUntracked = false
+	}
+
+	job.Cache = make(common.JRCaches, 1)
+	job.Cache[0] = common.JRCache{
+		Key:       cacheKey,
+		Untracked: cacheUntracked.(bool),
+		Paths:     paths,
+	}
+
+	return
+}
+
+func (c *GitLabCiYamlParser) ParseYaml(job *common.JobResponse) (err error) {
+	err = c.parseFile()
+	if err != nil {
+		return err
+	}
+
+	err = c.loadJob()
+	if err != nil {
+		return err
+	}
+
+	parsers := []struct {
+		method func(job *common.JobResponse) error
+	}{
+		{c.prepareJobInfo},
+		{c.prepareSteps},
+		{c.prepareVariables},
+		{c.prepareImage},
+		{c.prepareServices},
+		{c.prepareArtifacts},
+		{c.prepareCache},
+	}
+
+	for _, parser := range parsers {
+		err = parser.method(job)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func NewGitLabCiYamlParser(jobName string) *GitLabCiYamlParser {
+	return &GitLabCiYamlParser{
+		jobName: jobName,
+	}
+}
