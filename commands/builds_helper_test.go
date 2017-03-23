bcab5edc79d1d7f2b63807261123bcd6f716d474
Tomasz Maczukin <tomasz@maczukin.pl>
2017-03-23 03:44:37 +0100

    Add test simulating the error reported in #2228

--- commands/builds_helper_test.go
+++ commands/builds_helper_test.go
@@ -1,12 +1,14 @@
 package commands
 
 import (
+	"sync"
 	"testing"
+	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/stretchr/testify/assert"
-
 	"github.com/stretchr/testify/require"
+
 	"gitlab.com/gitlab-org/gitlab-ci-multi-runner/common"
 )
 
@@ -110,4 +112,85 @@ func TestBuildsHelperAcquireBuildUnlimited(t *testing.T) {
 
 	result = b.releaseBuild(&runner)
 	require.True(t, result)
+}
+
+var testBuildCurrentID int
+
+func getTestBuild() *common.Build {
+	testBuildCurrentID++
+
+	runner := common.RunnerConfig{}
+	runner.Token = "a1b2c3d4"
+	jobInfo := common.JobInfo{
+		ProjectID: 1,
+	}
+
+	build := &common.Build{}
+	build.ID = testBuildCurrentID
+	build.Runner = &runner
+	build.JobInfo = jobInfo
+
+	return build
+}
+
+func concurrentlyCallStatesAndStages(b *buildsHelper) {
+	for {
+		b.statesAndStages()
+	}
+}
+
+func handleTestBuild(wg *sync.WaitGroup, b *buildsHelper, build *common.Build) {
+	stages := []common.BuildStage{
+		common.BuildStagePrepare,
+		common.BuildStageGetSources,
+		common.BuildStageRestoreCache,
+		common.BuildStageDownloadArtifacts,
+		common.BuildStageUserScript,
+		common.BuildStageAfterScript,
+		common.BuildStageArchiveCache,
+		common.BuildStageUploadArtifacts,
+	}
+	states := []common.BuildRuntimeState{
+		common.BuildRunStatePending,
+		common.BuildRunRuntimeRunning,
+		common.BuildRunRuntimeFinished,
+	}
+
+	b.addBuild(build)
+	time.Sleep(10 * time.Millisecond)
+	for _, stage := range stages {
+		for _, state := range states {
+			build.CurrentStage = stage
+			build.CurrentState = state
+			time.Sleep(5 * time.Millisecond)
+		}
+	}
+	time.Sleep(5 * time.Millisecond)
+	b.removeBuild(build)
+
+	time.Sleep(5 * time.Millisecond)
+	wg.Done()
+}
+
+func TestBuildHelperCollectWhenRemovingBuild(t *testing.T) {
+	t.Log("This test tries to simulate concurrency. It can be false-positive! But failure is always a sign that something is wrong.")
+	b := &buildsHelper{}
+
+	for i := 0; i < 10; i++ {
+		go concurrentlyCallStatesAndStages(b)
+	}
+
+	var builds []*common.Build
+	for i := 1; i < 30; i++ {
+		builds = append(builds, getTestBuild())
+	}
+
+	wg := &sync.WaitGroup{}
+	wg.Add(len(builds))
+
+	for _, build := range builds {
+		go handleTestBuild(wg, b, build)
+	}
+
+	wg.Wait()
 }
