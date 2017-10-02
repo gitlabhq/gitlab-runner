b893856ab2850fd271e03e4181319193467b253a
Alessio Caiazza <acaiazza@gitlab.com>
2017-10-02 13:57:49 +0200

    Simplify `BuildSection` tests

--- helpers/build_section_test.go
+++ helpers/build_section_test.go
@@ -1,112 +1,58 @@
-package helpers
+package helpers_test
 
 import (
+	"bytes"
 	"fmt"
-	"regexp"
 	"testing"
-	"time"
 
 	"github.com/stretchr/testify/assert"
-	"strconv"
+
+	"gitlab.com/gitlab-org/gitlab-runner/helpers"
 )
 
-func newSectionParser() *sectionParser {
-	parser := &sectionParser{}
-	parser.SectionRegexp = regexp.MustCompile("section_((?:start)|(?:end)):(\\d+):(.+)\r\033\\[0K")
-	parser.Sections = make([]*parsedSection, 0)
-
-	return parser
+type testBuffer struct {
+	bytes.Buffer
+	Error error
 }
 
-type parsedSection struct {
-	Name  string
-	Start time.Time
-	End   time.Time
-}
-
-func (s *parsedSection) Duration() time.Duration {
-	return s.End.Sub(s.Start)
-}
-
-type sectionParser struct {
-	Sections      []*parsedSection
-	SectionRegexp *regexp.Regexp
-	Error         error
-
-	currentSection *parsedSection
-}
-
-func (p *sectionParser) SendRawLog(args ...interface{}) {
-	if p.Error != nil {
+func (b *testBuffer) SendRawLog(args ...interface{}) {
+	if b.Error != nil {
 		return
 	}
 
-	line := fmt.Sprintln(args...)
-	matches := p.SectionRegexp.FindStringSubmatch(line)
-	if matches != nil {
-		folding := matches[1]
-		epoch, err := strconv.ParseInt(matches[2], 10, 64)
-		if err != nil {
-			p.Error = err
-			return
-		}
-		section := matches[3]
-		timestamp := time.Unix(epoch, 0)
-
-		switch folding {
-		case "start":
-			if p.currentSection != nil {
-				p.Error = fmt.Errorf("Double opening: %s and %s", p.currentSection.Name, section)
-				return
-			}
-			p.currentSection = &parsedSection{Name: section, Start: timestamp}
-		case "end":
-			if p.currentSection == nil {
-				p.Error = fmt.Errorf("End without open: %s", section)
-				return
-			}
-
-			if p.currentSection.Name != section {
-				p.Error = fmt.Errorf("Section name mismatch: start %s - end %s", p.currentSection.Name, section)
-				return
-			}
-
-			p.currentSection.End = timestamp
-			p.Sections = append(p.Sections, p.currentSection)
-			p.currentSection = nil
-		}
-	}
+	_, b.Error = b.WriteString(fmt.Sprintln(args...))
 }
 
 func TestBuildSection(t *testing.T) {
 	for num, tc := range []struct {
-		name  string
-		delay time.Duration
-		error error
+		name        string
+		skipMetrics bool
+		error       error
 	}{
-		{"MyTest", time.Second, nil},
-		{"MyFailingTest", time.Second, fmt.Errorf("Failing test")},
-		{"0_time", 0 * time.Second, nil},
+		{"Success", false, nil},
+		{"Failure", false, fmt.Errorf("Failing test")},
+		{"SkipMetricsSuccess", true, nil},
+		{"SkipMetricsFailure", true, fmt.Errorf("Failing test")},
 	} {
-		parser := newSectionParser()
+		t.Run(tc.name, func(t *testing.T) {
+			logger := new(testBuffer)
 
-		section := BuildSection{Name: tc.name, Run: func() error { time.Sleep(tc.delay); return tc.error }}
-		section.Execute(parser)
+			section := helpers.BuildSection{
+				Name:        tc.name,
+				SkipMetrics: tc.skipMetrics,
+				Run:         func() error { return tc.error },
+			}
+			section.Execute(logger)
 
-		assert.Nil(t, parser.Error, "case %d: Error: %s", num, parser.Error)
-		assert.Equal(t, 1, len(parser.Sections), "case %d: wrong number of sections detected", num)
-		firstSection := parser.Sections[0]
-		assert.Equal(t, tc.name, firstSection.Name, "case %d: wrong name", num)
-		assert.Equal(t, tc.delay, firstSection.Duration(), "case %d: wrong duration")
+			output := logger.String()
+			assert.Nil(t, logger.Error, "case %d: Error: %s", num, logger.Error)
+			for _, str := range []string{"section_start:", "section_end:", tc.name} {
+				if tc.skipMetrics {
+					assert.NotContains(t, output, str)
+				} else {
+					assert.Contains(t, output, str)
+				}
+			}
+		})
 	}
-}
-
-func TestBuildSectionSkipMetrics(t *testing.T) {
-	parser := newSectionParser()
-
-	section := BuildSection{Name: "SkipMetrics", SkipMetrics: true, Run: func() error { return nil }}
-	section.Execute(parser)
-
-	assert.Nil(t, parser.Error)
-	assert.Equal(t, 0, len(parser.Sections))
 }
