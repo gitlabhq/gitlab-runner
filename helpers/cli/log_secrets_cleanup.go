a9d257fbecc89f9413715cce11e98d8db00fb802
Kamil Trzciński <ayufan@ayufan.eu>
2017-10-04 10:42:50 +0000

    Merge branch '2741-hide-security-tokens-from-output' into 'master'
    Hide tokens from URLs printed in job's trace
    
    Closes #2741
    
    See merge request gitlab-org/gitlab-runner!708

---
+++ helpers/cli/log_secrets_cleanup.go
@@ -0,0 +1,29 @@
+package cli_helpers
+
+import (
+	"github.com/Sirupsen/logrus"
+
+	"gitlab.com/gitlab-org/gitlab-ci-multi-runner/helpers/url"
+)
+
+type SecretsCleanupHook struct{}
+
+func (s *SecretsCleanupHook) Levels() []logrus.Level {
+	return []logrus.Level{
+		logrus.PanicLevel,
+		logrus.FatalLevel,
+		logrus.ErrorLevel,
+		logrus.WarnLevel,
+		logrus.InfoLevel,
+		logrus.DebugLevel,
+	}
+}
+
+func (s *SecretsCleanupHook) Fire(entry *logrus.Entry) error {
+	entry.Message = url_helpers.ScrubSecrets(entry.Message)
+	return nil
+}
+
+func AddSecretsCleanupLogHook() {
+	logrus.AddHook(&SecretsCleanupHook{})
+}
