e7665bee33b0815eb5c2abb54a10e1ce3abd591d
Tomasz Maczukin <tomasz@maczukin.pl>
2019-01-05 22:09:40 +0100

    Update release checklist issue generation script

--- scripts/prepare_release_checklist_issue.go
+++ scripts/prepare_release_checklist_issue.go
@@ -15,6 +15,8 @@ import (
 	"strings"
 	"text/template"
 	"time"
+
+	"gopkg.in/yaml.v2"
 )
 
 type ReleaseMetadata struct {
@@ -23,16 +25,22 @@ type ReleaseMetadata struct {
 	ReleaseManagerHandle    string
 	ReleaseBlogPostMR       int
 	ReleaseBlogPostDeadline string
+	HelmChartMajor          int
+	HelmChartMinor          int
+	HelmChartPatch          int
 }
 
 const (
-	GitLabRunnerProjectID = "gitlab-org/gitlab-runner"
-	WWWGitlabComProjectID = "gitlab-com/www-gitlab-com"
+	GitLabRunnerProjectID          = "gitlab-org/gitlab-runner"
+	GitLabRunnerHelmChartProjectID = "charts/gitlab-runner"
+	WWWGitlabComProjectID          = "gitlab-com/www-gitlab-com"
 
 	ReleasePostLabel = "release post"
 
 	ReleaseManagerHandleEnvVariable = "GITLAB_RUNNER_RELEASE_MANAGER_HANDLE"
-	VersionFile                     = "./VERSION"
+
+	VersionFile          = "./VERSION"
+	HelmChartVersionFile = "Chart.yaml"
 
 	LayoutDay = "2006-01-02"
 )
@@ -41,8 +49,9 @@ var (
 	reader          *bufio.Reader
 	releaseMetadata ReleaseMetadata
 
-	defaultVersion      []string
-	defaultMergeRequest []string
+	detectedVersion          []string
+	detectedMergeRequest     []string
+	detectedHelmChartVersion []string
 
 	templateFilePath = flag.String("issue-template-file", ".gitlab/issue_templates/Release Checklist.md", "Path to a file with issue template")
 
@@ -54,11 +63,15 @@ var (
 	releaseManagerHandle    = flag.String("release-manager-handle", defaultReleaseManagerHandle(), "GitLab.com handle of the release manager")
 	releaseBlogPostMR       = flag.String("release-blog-post-mr", detectBlogPostMergeRequest()[0], "ID of the Release Blog Post MR")
 	releaseBlogPostDeadline = flag.String("release-blog-post-deadline", detectReleaseMergeRequestDeadline(), "Deadline for adding Runner specific content to the Release Blog Post")
+
+	helmChartMajor = flag.String("helm-chart-major", detectHelmChartVersion()[0], "Major version number of GitLab Runner Helm Chart")
+	helmChartMinor = flag.String("helm-chart-minor", detectHelmChartVersion()[1], "Minor version number of GitLab Runner Helm Chart")
+	helmChartPatch = flag.String("helm-chart-patch", detectHelmChartVersion()[2], "Patch version number of GitLab Runner Helm Chart")
 )
 
 func detectVersion() []string {
-	if len(defaultVersion) > 0 {
-		return defaultVersion
+	if len(detectedVersion) > 0 {
+		return detectedVersion
 	}
 
 	fmt.Println("Auto-detecting version...")
@@ -72,9 +85,65 @@ func detectVersion() []string {
 
 	fmt.Printf("Found: %s\n", content)
 
-	defaultVersion = strings.Split(string(content), ".")
+	detectedVersion = strings.Split(string(content), ".")
 
-	return defaultVersion
+	return detectedVersion
+}
+
+type HelmChartData struct {
+	Version string `yaml:"version"`
+}
+
+func detectHelmChartVersion() []string {
+	if len(detectedHelmChartVersion) > 0 {
+		return detectedHelmChartVersion
+	}
+
+	fmt.Println("Auto-detecting Helm Chart version...")
+
+	charVersionFileURL := fmt.Sprintf("https://gitlab.com/%s/raw/master/%s", GitLabRunnerHelmChartProjectID, HelmChartVersionFile)
+	req, err := http.NewRequest(http.MethodGet, charVersionFileURL, nil)
+	if err != nil {
+		panic(fmt.Errorf("error while creating helm chart version detection request: %v", err))
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		panic(fmt.Errorf("error while requesting helm chart version: %v", err))
+	}
+
+	defer resp.Body.Close()
+	content, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(fmt.Errorf("error while reading helm chart version response: %v", err))
+	}
+
+	helmChartData := new(HelmChartData)
+	err = yaml.Unmarshal(content, helmChartData)
+	if err != nil {
+		fmt.Printf("Error while parsing Helm Chart version file: %v", err)
+
+		return []string{"", "", ""}
+	}
+
+	fmt.Printf("Found: %s\n", helmChartData.Version)
+
+	versionRx := regexp.MustCompile(`([0-9]+)\.([0-9]+)\.([0-9]+)`)
+	versions := versionRx.FindAllStringSubmatch(helmChartData.Version, -1)
+
+	if len(versions) != 1 {
+		fmt.Printf("Couldn't parse the version string")
+
+		return []string{"", "", ""}
+	}
+
+	detectedHelmChartVersion = []string{
+		versions[0][1],
+		versions[0][2],
+		versions[0][3],
+	}
+
+	return detectedHelmChartVersion
 }
 
 func defaultReleaseManagerHandle() string {
@@ -96,8 +165,8 @@ type listMergeRequestsResponseEntry struct {
 }
 
 func detectBlogPostMergeRequest() []string {
-	if len(defaultMergeRequest) > 0 {
-		return defaultMergeRequest
+	if len(detectedMergeRequest) > 0 {
+		return detectedMergeRequest
 	}
 
 	fmt.Println("Auto-detecting Release Post merge request...")
@@ -171,12 +240,12 @@ func detectBlogPostMergeRequest() []string {
 	r := regexp.MustCompile("gitlab.com/gitlab-com/www-gitlab-com/blob/release-\\d+-\\d+/data/release_posts/(\\d+)_(\\d+)_(\\d+)_gitlab_\\d+_\\d+_released.yml")
 	dateParts := r.FindStringSubmatch(chosen.Description)
 
-	defaultMergeRequest = []string{
+	detectedMergeRequest = []string{
 		strconv.Itoa(chosen.ID),
 		fmt.Sprintf("%s-%s-%s", dateParts[1], dateParts[2], dateParts[3]),
 	}
 
-	return defaultMergeRequest
+	return detectedMergeRequest
 }
 
 func detectReleaseMergeRequestDeadline() string {
@@ -243,40 +312,55 @@ func main() {
 func prepareMetadata() {
 	var err error
 
-	askOnce("Major version number:", major)
+	askOnce("Major version number", major)
 	releaseMetadata.Major, err = strconv.Atoi(*major)
 	if err != nil {
 		panic(err)
 	}
 
-	askOnce("Minor version number:", minor)
+	askOnce("Minor version number", minor)
 	releaseMetadata.Minor, err = strconv.Atoi(*minor)
 	if err != nil {
 		panic(err)
 	}
 
-	askOnce("GitLab.com handle of the release manager:", releaseManagerHandle)
+	askOnce("Helm Chart Major version number", helmChartMajor)
+	releaseMetadata.HelmChartMajor, err = strconv.Atoi(*helmChartMajor)
+	if err != nil {
+		panic(err)
+	}
+
+	askOnce("Helm Chart Minor version number", helmChartMinor)
+	releaseMetadata.HelmChartMinor, err = strconv.Atoi(*helmChartMinor)
+	if err != nil {
+		panic(err)
+	}
+
+	askOnce("Helm Chart Patch version number", helmChartPatch)
+	releaseMetadata.HelmChartPatch, err = strconv.Atoi(*helmChartPatch)
+	if err != nil {
+		panic(err)
+	}
+
+	askOnce("GitLab.com handle of the release manager", releaseManagerHandle)
 	releaseMetadata.ReleaseManagerHandle = *releaseManagerHandle
 
-	askOnce("ID of the Release Blog Post MR:", releaseBlogPostMR)
+	askOnce("ID of the Release Blog Post MR", releaseBlogPostMR)
 	releaseMetadata.ReleaseBlogPostMR, err = strconv.Atoi(*releaseBlogPostMR)
 	if err != nil {
 		panic(err)
 	}
 
-	askOnce("Deadline for adding Runner specific content to the Release Blog Post:", releaseBlogPostDeadline)
+	askOnce("Deadline for adding Runner specific content to the Release Blog Post", releaseBlogPostDeadline)
 	releaseMetadata.ReleaseBlogPostDeadline = *releaseBlogPostDeadline
 }
 
 func askOnce(prompt string, result *string) {
+	fmt.Printf("%s [%s]: ", prompt, *result)
 	if *noInteractive {
+		fmt.Println()
+
 		return
-	}
-
-	fmt.Println(prompt)
-
-	if *result != "" {
-		fmt.Printf("[%s]: ", *result)
 	}
 
 	if reader == nil {
