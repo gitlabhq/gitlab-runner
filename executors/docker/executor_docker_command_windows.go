a419fbe44f2668e3fa7c65c25239c4ce4eeaf109
Tomasz Maczukin <tomasz@gitlab.com>
2019-05-24 11:56:47 +0000

    Merge branch 'make-docker-windows-to-work-only-on-windows' into 'master'
    Limit `docker-windows` to Windows
    
    Closes #4255
    
    See merge request gitlab-org/gitlab-runner!1362

---
+++ executors/docker/executor_docker_command_windows.go
@@ -0,0 +1,50 @@
+package docker
+
+import (
+	"gitlab.com/gitlab-org/gitlab-runner/common"
+	"gitlab.com/gitlab-org/gitlab-runner/executors"
+)
+
+func init() {
+	options := executors.ExecutorOptions{
+		DefaultCustomBuildsDirEnabled: true,
+		DefaultBuildsDir:              `c:\builds`,
+		DefaultCacheDir:               `c:\cache`,
+		SharedBuildsDir:               false,
+		Shell: common.ShellScriptInfo{
+			Shell:         "powershell",
+			Type:          common.NormalShell,
+			RunnerCommand: "gitlab-runner-helper",
+		},
+		ShowHostname: true,
+		Metadata: map[string]string{
+			"OSType": osTypeWindows,
+		},
+	}
+
+	creator := func() common.Executor {
+		e := &commandExecutor{
+			executor: executor{
+				AbstractExecutor: executors.AbstractExecutor{
+					ExecutorOptions: options,
+				},
+			},
+		}
+		e.SetCurrentStage(common.ExecutorStageCreated)
+		return e
+	}
+
+	featuresUpdater := func(features *common.FeaturesInfo) {
+		features.Variables = true
+		features.Image = true
+		features.Services = true
+		features.Session = false
+		features.Terminal = false
+	}
+
+	common.RegisterExecutor("docker-windows", executors.DefaultExecutorProvider{
+		Creator:          creator,
+		FeaturesUpdater:  featuresUpdater,
+		DefaultShellName: options.Shell.Shell,
+	})
+}
