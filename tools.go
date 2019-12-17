ae88ee1923443377b6641f883f5cf7afe372da0f
Steve Azzopardi <sazzopardi@gitlab.com>
2019-12-17 09:56:02 +0100

    Save dev modules go.mod/sum
    Modules such as `github.com/vektra/mockery` are only used for
    development when an engineer runs `make mocks`. This causes a change in
    `go.mod/sum` files everytime, and the engineer either has to do a `go
    mod tidy` of ignore the git diff. This also breaks down for problems
    such as https://gitlab.com/gitlab-org/gitlab-runner/merge_requests/1651#note_261532242
    
    Adding a tools.go behind a build flag prevents such problems, as
    explained in https://marcofranssen.nl/manage-go-tools-via-go-modules/

---
+++ tools.go
@@ -0,0 +1,11 @@
+// +build tools
+
+package main
+
+// These imports are to force `go mod tidy` not to remove that tools we depend
+// on development. This is explained in great detail in
+// https://marcofranssen.nl/manage-go-tools-via-go-modules/
+import (
+	_ "github.com/mitchellh/gox"
+	_ "github.com/vektra/mockery/cmd/mockery"
+)
