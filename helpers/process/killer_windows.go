969ff7d8140e308eb95785e67c94d3a53041d4d9
Steve Azzopardi <sazzopardi@gitlab.com>
2020-01-29 08:53:01 +0100

    Use taskkill for process termination on Windows
    Calling process.Kill will only send the kill signal to the parent
    process, but will not send the process signal to the child process.
    Using `taskkill` directory we can send the signal to child processes
    using the `/T` flag. With `ForceKill` we also pass the `/F` flag to
    force kill a process.
    
    This also makes is consistent with the [shell executor
    termination](https://gitlab.com/gitlab-org/gitlab-runner/blob/bfa18b82581b5afc79eff609260cec5db612dc79/helpers/process_group_windows.go#L16)
    
    To support graceful termination on Windows we will to this in
    https://gitlab.com/gitlab-org/gitlab-runner/issues/3121 since the goal
    of this commit is to make it consistent with the Shell executor
    
    Reference: https://gitlab.com/gitlab-org/gitlab-runner/issues/3376
    Reference: https://gitlab.com/gitlab-org/gitlab-runner/issues/4438

--- helpers/process/killer_windows.go
+++ helpers/process/killer_windows.go
@@ -1,5 +1,10 @@
 package process
 
+import (
+	"os/exec"
+	"strconv"
+)
+
 type windowsKiller struct {
 	logger Logger
 	cmd    Commander
@@ -12,12 +17,15 @@ func newKiller(logger Logger, cmd Commander) killer {
 	}
 }
 
+// Terminate on windows sends a taskkill to the command and it's child processes
+// forcefully `/F` since most time the process can't be killed and ends up
+// erroring out.
 func (pk *windowsKiller) Terminate() {
 	if pk.cmd.Process() == nil {
 		return
 	}
 
-	err := pk.cmd.Process().Kill()
+	err := taskKill(pk.cmd.Process().Pid)
 	if err != nil {
 		pk.logger.Warn("Failed to terminate process:", err)
 
@@ -31,8 +39,12 @@ func (pk *windowsKiller) ForceKill() {
 		return
 	}
 
-	err := pk.cmd.Process().Kill()
+	err := taskKill(pk.cmd.Process().Pid)
 	if err != nil {
 		pk.logger.Warn("Failed to force-kill:", err)
 	}
+}
+
+func taskKill(pid int) error {
+	return exec.Command("taskkill", "/F", "/T", "/PID", strconv.Itoa(pid)).Run()
 }
