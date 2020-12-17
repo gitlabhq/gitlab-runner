9423674651b60216333a68e1a504553be7747b45
Pedro Pombeiro <noreply@pedro.pombei.ro>
2020-12-17 15:51:53 +0100

    Add tests to Powershell shell

--- shells/powershell_test.go
+++ shells/powershell_test.go
@@ -4,6 +4,9 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"gitlab.com/gitlab-org/gitlab-runner/common"
 )
 
 func TestPowershell_LineBreaks(t *testing.T) {
@@ -79,4 +82,85 @@ func TestPowershell_MkTmpDirOnUNCShare(t *testing.T) {
 		`New-Item -ItemType directory -Force -Path "\\unc-server\share\tmp" | out-null`+writer.EOL,
 		writer.String(),
 	)
+}
+
+func TestPowershell_GetName(t *testing.T) {
+	for _, shellName := range []string{SNPwsh, SNPowershell} {
+		shell := common.GetShell(shellName)
+		assert.Equal(t, shellName, shell.GetName())
+	}
+}
+
+func TestPowershell_IsDefault(t *testing.T) {
+	for _, shellName := range []string{SNPwsh, SNPowershell} {
+		shell := common.GetShell(shellName)
+		assert.False(t, shell.IsDefault())
+	}
+}
+
+func TestPowershell_GetConfiguration(t *testing.T) {
+	testCases := map[string]struct {
+		shell    string
+		executor string
+
+		expectedPassFile bool
+	}{
+		"powershell on docker-windows": {
+			shell:            SNPowershell,
+			executor:         dockerWindowsExecutor,
+			expectedPassFile: false,
+		},
+		"pwsh on docker-windows": {
+			shell:            SNPwsh,
+			executor:         dockerWindowsExecutor,
+			expectedPassFile: false,
+		},
+		"pwsh on shell": {
+			shell:            SNPwsh,
+			executor:         "shell",
+			expectedPassFile: true,
+		},
+	}
+
+	for tn, tc := range testCases {
+		t.Run(tn, func(t *testing.T) {
+			shell := common.GetShell(tc.shell)
+			info := common.ShellScriptInfo{
+				Shell: tc.shell,
+				Build: &common.Build{
+					Runner: &common.RunnerConfig{},
+				},
+			}
+			info.Build.Runner.Executor = tc.executor
+
+			shellConfig, err := shell.GetConfiguration(info)
+			require.NoError(t, err)
+			assert.Equal(t, tc.shell, shellConfig.Command)
+			assert.Equal(
+				t,
+				[]string{"-NoProfile", "-NonInteractive", "-ExecutionPolicy", "Bypass", "-Command"},
+				shellConfig.Arguments,
+			)
+			assert.Equal(
+				t,
+				[]string{
+					tc.shell,
+					"-NoProfile",
+					"-NoLogo",
+					"-InputFormat",
+					"text",
+					"-OutputFormat",
+					"text",
+					"-NonInteractive",
+					"-ExecutionPolicy",
+					"Bypass",
+					"-Command",
+					"-",
+				},
+				shellConfig.DockerCommand,
+			)
+			assert.Equal(t, tc.expectedPassFile, shellConfig.PassFile)
+			assert.Equal(t, "ps1", shellConfig.Extension)
+		})
+	}
 }
