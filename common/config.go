1fa44f1f15cd993d1dec72c9a1db0e04e1a23000
bbutler38 <brandon.butler38@t-mobile.com>
2021-02-26 21:50:40 -0600

    Add support for CSI volumes

--- common/config.go
+++ common/config.go
@@ -349,6 +349,7 @@ type KubernetesVolumes struct {
 	ConfigMaps []KubernetesConfigMap `toml:"config_map" description:"The config maps which will be mounted as volumes"`
 	Secrets    []KubernetesSecret    `toml:"secret" description:"The secret maps which will be mounted"`
 	EmptyDirs  []KubernetesEmptyDir  `toml:"empty_dir" description:"The empty dirs which will be mounted"`
+	CSIs       []KubernetesCSI       `toml:"csi" description:"The CSI volumes which will be mounted"`
 }
 
 //nolint:lll
@@ -392,6 +393,16 @@ type KubernetesEmptyDir struct {
 	MountPath string `toml:"mount_path" description:"Path where volume should be mounted inside of container"`
 	SubPath   string `toml:"sub_path,omitempty" description:"The sub-path of the volume to mount (defaults to volume root)"`
 	Medium    string `toml:"medium,omitempty" description:"Set to 'Memory' to have a tmpfs"`
+}
+
+//nolint:lll
+type KubernetesCSI struct {
+	Name             string            `toml:"name" json:"name" description:"The name of the CSI volume and volumeMount to use"`
+	MountPath        string            `toml:"mount_path" description:"Path where volume should be mounted inside of container"`
+	Driver           string            `toml:"driver" description:"Path where volume should be mounted inside of container"`
+	SubPath          string            `toml:"sub_path,omitempty" description:"The sub-path of the volume to mount (defaults to volume root)"`
+	ReadOnly         bool              `toml:"read_only,omitempty" description:"If this volume should be mounted read only"`
+	VolumeAttributes map[string]string `toml:"volume_attributes,omitempty" description:"Key-to-path mapping for attributes of the CSI volume."`
 }
 
 //nolint:lll
