b78759c908b533df9dcb4f6bdd8af87e3c734c78
Pedro Pombeiro <noreply@pedro.pombei.ro>
2021-04-19 16:02:24 +0200

    Address MR review comments

--- commands/register_test.go
+++ commands/register_test.go
@@ -1,23 +1,16 @@
 package commands
 
 import (
-	"bufio"
 	"flag"
-	"fmt"
 	"io/ioutil"
 	"os"
-	"strings"
 	"testing"
 
 	"github.com/imdario/mergo"
 	"github.com/pkg/errors"
-	"github.com/sirupsen/logrus"
-	"github.com/sirupsen/logrus/hooks/test"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 	"github.com/urfave/cli"
-	clihelpers "gitlab.com/ayufan/golang-cli-helpers"
 
 	"gitlab.com/gitlab-org/gitlab-runner/common"
 	_ "gitlab.com/gitlab-org/gitlab-runner/executors/docker/machine"
@@ -80,118 +73,6 @@ func TestRegisterCustomMappedDockerCacheVolume(t *testing.T) {
 
 	assert.Equal(t, 1, len(s.Docker.Volumes))
 	assert.Equal(t, "/my/cache:/cache", s.Docker.Volumes[0])
-}
-
-func testRegisterCommandRun(
-	t *testing.T,
-	network common.Network,
-	args ...string,
-) (content, output string, err error) {
-	hook := test.NewGlobal()
-
-	defer func() {
-		output = GetLogrusOutput(t, hook)
-
-		if r := recover(); r != nil {
-			// log panics forces exit
-			if e, ok := r.(*logrus.Entry); ok {
-				err = fmt.Errorf("command error: %s", e.Message)
-			}
-		}
-	}()
-
-	cmd := newRegisterCommand()
-	cmd.network = network
-
-	app := cli.NewApp()
-	app.Commands = []cli.Command{
-		{
-			Name:   "register",
-			Action: cmd.Execute,
-			Flags:  clihelpers.GetFlagsFromStruct(cmd),
-		},
-	}
-
-	configFile, err := ioutil.TempFile("", "config.toml")
-	require.NoError(t, err)
-
-	err = configFile.Close()
-	require.NoError(t, err)
-
-	defer os.Remove(configFile.Name())
-
-	args = append([]string{
-		"binary", "register",
-		"-n",
-		"--config", configFile.Name(),
-		"--url", "http://gitlab.example.com/",
-		"--registration-token", "test-registration-token",
-		"--executor", "shell",
-	}, args...)
-
-	comandErr := app.Run(args)
-
-	fileContent, err := ioutil.ReadFile(configFile.Name())
-	require.NoError(t, err)
-
-	err = comandErr
-
-	return string(fileContent), "", err
-}
-
-func TestAccessLevelSetting(t *testing.T) {
-	tests := map[string]struct {
-		accessLevel     AccessLevel
-		failureExpected bool
-	}{
-		"access level not defined": {},
-		"ref_protected used": {
-			accessLevel: RefProtected,
-		},
-		"not_protected used": {
-			accessLevel: NotProtected,
-		},
-		"unknown access level": {
-			accessLevel:     AccessLevel("unknown"),
-			failureExpected: true,
-		},
-	}
-
-	for testName, testCase := range tests {
-		t.Run(testName, func(t *testing.T) {
-			network := new(common.MockNetwork)
-			defer network.AssertExpectations(t)
-
-			if !testCase.failureExpected {
-				parametersMocker := mock.MatchedBy(func(parameters common.RegisterRunnerParameters) bool {
-					return AccessLevel(parameters.AccessLevel) == testCase.accessLevel
-				})
-
-				network.On("RegisterRunner", mock.Anything, parametersMocker).
-					Return(&common.RegisterRunnerResponse{
-						Token: "test-runner-token",
-					}).
-					Once()
-			}
-
-			arguments := []string{
-				"--access-level", string(testCase.accessLevel),
-			}
-
-			_, output, err := testRegisterCommandRun(t, network, arguments...)
-
-			if testCase.failureExpected {
-				assert.EqualError(t, err, "command error: Given access-level is not valid. "+
-					"Refer to gitlab-runner register -h for the correct options.")
-				assert.NotContains(t, output, "Runner registered successfully.")
-
-				return
-			}
-
-			assert.NoError(t, err)
-			assert.Contains(t, output, "Runner registered successfully.")
-		})
-	}
 }
 
 func TestConfigTemplate_Enabled(t *testing.T) {
@@ -348,107 +229,4 @@ func TestConfigTemplate_MergeTo(t *testing.T) {
 			tc.assertConfiguration(t, tc.config)
 		})
 	}
-}
-
-func TestUnregisterOnFailure(t *testing.T) {
-	tests := map[string]struct {
-		leaveRunner           bool
-		registrationFails     bool
-		expectsLeftRegistered bool
-	}{
-		"registration succeeds, runner left registered": {
-			leaveRunner:           false,
-			registrationFails:     false,
-			expectsLeftRegistered: true,
-		},
-		"registration fails, LeaveRunner is false, runner is unregistered": {
-			leaveRunner:           false,
-			registrationFails:     true,
-			expectsLeftRegistered: false,
-		},
-		"registration fails, LeaveRunner is true, runner left registered": {
-			leaveRunner:           true,
-			registrationFails:     true,
-			expectsLeftRegistered: true,
-		},
-	}
-
-	for testName, testCase := range tests {
-		t.Run(testName, func(t *testing.T) {
-			network := new(common.MockNetwork)
-			defer network.AssertExpectations(t)
-
-			const token = "test-runner-token"
-			network.On("RegisterRunner", mock.Anything, mock.Anything).
-				Return(&common.RegisterRunnerResponse{
-					Token: token,
-				}).
-				Once()
-			if !testCase.expectsLeftRegistered {
-				credsMocker := mock.MatchedBy(func(credentials common.RunnerCredentials) bool {
-					return credentials.Token == token
-				})
-				network.On("UnregisterRunner", credsMocker).
-					Return(true).
-					Once()
-			}
-
-			var arguments []string
-			if testCase.leaveRunner {
-				arguments = append(arguments, "--leave-runner")
-			}
-
-			answers := []string{"https://gitlab.com/", token, "description", ""}
-			if testCase.registrationFails {
-				defer func() { _ = recover() }()
-			} else {
-				answers = append(answers, "custom") // should not result in more answers required
-			}
-			cmd := newRegisterCommand()
-			cmd.reader = bufio.NewReader(strings.NewReader(strings.Join(answers, "\n") + "\n"))
-			cmd.network = network
-
-			app := cli.NewApp()
-			app.Commands = []cli.Command{
-				{
-					Name:   "register",
-					Action: cmd.Execute,
-					Flags:  clihelpers.GetFlagsFromStruct(cmd),
-				},
-			}
-
-			err := app.Run(append([]string{"runner", "register"}, arguments...))
-
-			assert.False(t, testCase.registrationFails)
-			assert.NoError(t, err)
-		})
-	}
-}
-
-func TestRegisterCommand_FeatureFlag(t *testing.T) {
-	expectedConfig := `
-  [runners.feature_flags]
-    FF_TEST_1 = true
-    FF_TEST_2 = false
-`
-
-	network := new(common.MockNetwork)
-	defer network.AssertExpectations(t)
-
-	network.On("RegisterRunner", mock.Anything, mock.Anything).
-		Return(&common.RegisterRunnerResponse{
-			Token: "test-runner-token",
-		}).
-		Once()
-
-	arguments := []string{
-		"--name", "test-runner",
-		"--feature-flags", "FF_TEST_1:true",
-		"--feature-flags", "FF_TEST_2:false",
-	}
-
-	gotConfig, _, err := testRegisterCommandRun(t, network, arguments...)
-	require.NoError(t, err)
-
-	assert.Contains(t, gotConfig, expectedConfig)
 }
