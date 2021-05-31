92649723d29155cd4eaec74f04157bc472667831
Arran Walker <ajwalker@gitlab.com>
2021-05-31 18:47:52 +0100

    Use Splitic test runner

--- common/build_test.go
+++ common/build_test.go
@@ -32,6 +32,7 @@ import (
 func init() {
 	s := MockShell{}
 	s.On("GetName").Return("script-shell")
+	s.On("IsDefault").Return(false)
 	s.On("GenerateScript", mock.Anything, mock.Anything).Return("script", nil)
 	RegisterShell(&s)
 }
