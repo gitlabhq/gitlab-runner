a44af4e980c4573899ea323325c1c7628af71a88
ggeorgiev_ <ggeorgiev@gitlab.com>
2021-07-04 02:36:15 +0300

    Allow KUBECONFIG and GITLAB_CI in tests

--- main_test.go
+++ main_test.go
@@ -13,7 +13,6 @@ const failure = `Environment variables from GitLab detected in tests,
 these should be cleared: https://gitlab.com/gitlab-org/gitlab-runner/-/issues/27965`
 
 func TestEnvVariablesCleaned(t *testing.T) {
-	assert.Empty(t, os.Getenv("CI_GITLAB"), failure)
 	assert.Empty(t, os.Getenv("CI_API_V4_URL"), failure)
 	assert.NotEmpty(t, os.Getenv("CI"), "If running locally, use `export CI=0` explicitly.")
 }
