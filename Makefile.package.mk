42d9a075244e0bf351588f63282af63c126d8d85
Steve Azzopardi <sazzopardi@gitlab.com>
2021-08-23 10:07:05 +0200

    ci: split linux packages into multiple jobs
    Background
    ---
    In https://gitlab.com/gitlab-org/gitlab-runner/-/issues/28149 we are
    seeing jobs failing because of large artifact sizes as described in
    https://gitlab.com/gitlab-com/gl-infra/infrastructure/-/issues/13249.
    `gitlab-org/gitlab-runner` already has a specific setting to override
    the default [1GB
    limit](https://docs.gitlab.com/ee/user/gitlab_com/index.html#gitlab-cicd).
    In https://gitlab.com/gitlab-org/gitlab-runner/-/merge_requests/2868 we
    also are planning to add another architecture which will increase the
    artifact size once more.
    
    Implentation
    ---
    Use a [matrix](https://docs.gitlab.com/ee/ci/yaml/#parallel-matrix-jobs)
    instead of copy/pasting the values for build and package.
    
    reference https://gitlab.com/gitlab-org/gitlab-runner/-/issues/28149

--- Makefile.package.mk
+++ Makefile.package.mk
@@ -11,27 +11,37 @@ package-prepare:
 	chmod 755 packaging/root/usr/share/gitlab-runner/
 	chmod 755 packaging/root/usr/share/gitlab-runner/*
 
-.PHONY: package-deb
-package-deb: package-deps package-prepare
-	# Building Debian compatible packages...
+.PHONY: package-deb-64bit
+package-deb-64bit: package-deps package-prepare
 	$(MAKE) package-deb-arch ARCH=amd64 PACKAGE_ARCH=amd64
-	$(MAKE) package-deb-arch ARCH=386 PACKAGE_ARCH=i386
 	$(MAKE) package-deb-arch ARCH=arm64 PACKAGE_ARCH=aarch64
 	$(MAKE) package-deb-arch ARCH=arm64 PACKAGE_ARCH=arm64
+
+.PHONY: package-deb-32bit
+package-deb-32bit: package-deps package-prepare
+	$(MAKE) package-deb-arch ARCH=386 PACKAGE_ARCH=i386
 	$(MAKE) package-deb-arch ARCH=arm PACKAGE_ARCH=armel
 	$(MAKE) package-deb-arch ARCH=arm PACKAGE_ARCH=armhf
+
+.PHONY: package-deb-ibm
+package-deb-ibm: package-deps package-prepare
 	$(MAKE) package-deb-arch ARCH=s390x PACKAGE_ARCH=s390x
 	$(MAKE) package-deb-arch ARCH=ppc64le PACKAGE_ARCH=ppc64el
 
-.PHONY: package-rpm
-package-rpm: package-deps package-prepare
-	# Building RedHat compatible packages...
+.PHONY: package-rpm-64bit
+package-rpm-64bit: package-deps package-prepare
 	$(MAKE) package-rpm-arch ARCH=amd64 PACKAGE_ARCH=amd64
-	$(MAKE) package-rpm-arch ARCH=386 PACKAGE_ARCH=i686
 	$(MAKE) package-rpm-arch ARCH=arm64 PACKAGE_ARCH=aarch64
 	$(MAKE) package-rpm-arch ARCH=arm64 PACKAGE_ARCH=arm64
+
+.PHONY: package-rpm-32bit
+package-rpm-32bit: package-deps package-prepare
+	$(MAKE) package-rpm-arch ARCH=386 PACKAGE_ARCH=i686
 	$(MAKE) package-rpm-arch ARCH=arm PACKAGE_ARCH=arm
 	$(MAKE) package-rpm-arch ARCH=arm PACKAGE_ARCH=armhf
+
+.PHONY: package-rpm-ibm
+package-rpm-ibm: package-deps package-prepare
 	$(MAKE) package-rpm-arch ARCH=s390x PACKAGE_ARCH=s390x
 	$(MAKE) package-rpm-arch ARCH=ppc64le PACKAGE_ARCH=ppc64le
 
