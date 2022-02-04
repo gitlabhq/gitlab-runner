23265b7b93f793bc3a3013c65a6300061d3273c8
Tomasz Maczukin <tomasz@maczukin.pl>
2022-02-04 20:39:10 +0100

    Add integration test for artifacts redirection

---
+++ commands/helpers/artifacts_uploader_integration_test.go
@@ -0,0 +1,124 @@
+//go:build integration
+// +build integration
+
+package helpers
+
+import (
+	"context"
+	"fmt"
+	"io/fs"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/urfave/cli"
+
+	"gitlab.com/gitlab-org/gitlab-runner/commands/helpers/archive/fastzip"
+	"gitlab.com/gitlab-org/gitlab-runner/common"
+	"gitlab.com/gitlab-org/gitlab-runner/helpers"
+	"gitlab.com/gitlab-org/gitlab-runner/network"
+)
+
+func TestArchiveUploadRedirect(t *testing.T) {
+	finalRequestReceived := false
+
+	finalServer := httptest.NewServer(finalServerHandler(t, &finalRequestReceived))
+	defer finalServer.Close()
+
+	redirectingServer := httptest.NewServer(redirectingServerHandler(finalServer.URL))
+	defer redirectingServer.Close()
+
+	cmd := &ArtifactsUploaderCommand{
+		JobCredentials: common.JobCredentials{
+			ID:    12345,
+			Token: "token",
+			URL:   redirectingServer.URL,
+		},
+		Name:             "artifacts",
+		Format:           common.ArtifactFormatZip,
+		CompressionLevel: "fastest",
+		network:          network.NewGitLabClient(),
+		fileArchiver: fileArchiver{
+			Paths: []string{
+				filepath.Join(".", "testdata", "test-artifacts"),
+			},
+		},
+	}
+
+	defer helpers.MakeFatalToPanic()()
+
+	assert.NotPanics(t, func() {
+		cmd.Execute(&cli.Context{})
+	}, "expected command not to log fatal")
+
+	assert.True(t, finalRequestReceived)
+}
+
+func redirectingServerHandler(finalServerURL string) http.HandlerFunc {
+	return func(rw http.ResponseWriter, r *http.Request) {
+		rw.Header().Set("Location", fmt.Sprintf("%s%s", finalServerURL, r.RequestURI))
+		rw.WriteHeader(http.StatusTemporaryRedirect)
+	}
+}
+
+func finalServerHandler(t *testing.T, finalRequestReceived *bool) http.HandlerFunc {
+	return func(rw http.ResponseWriter, r *http.Request) {
+		dir, err := os.MkdirTemp("", "test-archive-upload-redirect-dir-")
+		require.NoError(t, err)
+		defer func() {
+			_ = os.RemoveAll(dir)
+		}()
+
+		receiveFile(t, r, dir)
+
+		err = filepath.Walk(dir, func(path string, info fs.FileInfo, err error) error {
+			if info.IsDir() {
+				return nil
+			}
+
+			fileName := info.Name()
+			fileContentBytes, err := os.ReadFile(path)
+			if err != nil {
+				return err
+			}
+
+			assert.Equal(t, fileName, strings.TrimSpace(string(fileContentBytes)))
+
+			return nil
+		})
+
+		assert.NoError(t, err)
+
+		*finalRequestReceived = true
+		rw.WriteHeader(http.StatusCreated)
+	}
+}
+
+func receiveFile(t *testing.T, r *http.Request, targetDir string)  {
+	err := r.ParseMultipartForm(1024)
+	require.NoError(t, err)
+
+	formFiles := r.MultipartForm.File["file"]
+	require.Len(t, formFiles, 1)
+
+	formFile := formFiles[0]
+
+	assert.Equal(t, "artifacts.zip", formFile.Filename)
+
+	f, err := formFile.Open()
+	require.NoError(t, err)
+	defer func() {
+		_ = f.Close()
+	}()
+
+	extractor, err := fastzip.NewExtractor(f, formFile.Size, targetDir)
+	require.NoError(t, err)
+
+	err = extractor.Extract(context.Background())
+	require.NoError(t, err)
+}
