0fe75c17b317afb02476969bcf13eebabfa3299f
Pedro Pombeiro <noreply@pedro.pombei.ro>
2022-03-23 13:51:00 +0000

    Increase token short length if it includes prefix

--- helpers/shorten_token.go
+++ helpers/shorten_token.go
@@ -1,8 +1,24 @@
 package helpers
 
+import (
+	"strings"
+)
+
 func ShortenToken(token string) string {
-	if len(token) >= 8 {
-		return token[0:8]
+	if len(token) < 8 {
+		return token
 	}
-	return token
+
+	if token[:2] == "GR" && len(token) >= 17 {
+		// Token is prefixed with RUNNERS_TOKEN_PREFIX: GR (for Gitlab Runner) combined with the rotation
+		// date decimal-to-hex-encoded. Let's add some more characters in order to compensate.
+		if strings.IndexFunc(token[2:9], isInvalidPrefixRune) == -1 {
+			return token[:17]
+		}
+	}
+	return token[:8]
+}
+
+func isInvalidPrefixRune(r rune) bool {
+	return (r < '0' || r > '9') && (r < 'A' || r > 'F') && (r < 'a' || r > 'f')
 }
