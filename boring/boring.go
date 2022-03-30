a38e3ddbb2175266cb2b84e6d85b59da3a591b8e
Georgi N. Georgiev <ggeorgiev@gitlab.com>
2022-03-30 22:09:27 +0300

    Add a check whether boringssl is being used by using the Enabled method
    fail if BoringSSL is not enabled
    
    print a mesage whether boring is actually enabled

--- boring/boring.go
+++ boring/boring.go
@@ -3,8 +3,16 @@
 
 package boring
 
-import "fmt"
+import (
+	"crypto/boring"
+	"fmt"
+)
 
 func CheckBoring() {
-	fmt.Println("FIPS mode enabled. Using BoringSSL.")
+	if boring.Enabled() {
+		fmt.Println("FIPS mode enabled. Using BoringSSL.")
+		return
+	}
+
+	fmt.Println("GitLab Runner was compiled with FIPS mode but BoringSSL is not enabled.")
 }
