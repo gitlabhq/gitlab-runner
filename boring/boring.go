8ef434a9cbfe1a21efe3b37f2355dcc8c0cdabab
Georgi N. Georgiev <ggeorgiev@gitlab.com>
2022-07-25 19:26:36 +0300

    Print BoringSSL messages as warnings

--- boring/boring.go
+++ boring/boring.go
@@ -5,14 +5,14 @@ package boring
 
 import (
 	"crypto/boring"
-	"fmt"
+	"github.com/sirupsen/logrus"
 )
 
 func CheckBoring() {
 	if boring.Enabled() {
-		fmt.Println("FIPS mode enabled. Using BoringSSL.")
+		logrus.Warnln("FIPS mode enabled. Using BoringSSL.")
 		return
 	}
 
-	fmt.Println("GitLab Runner was compiled with FIPS mode but BoringSSL is not enabled.")
+	logrus.Warnln("GitLab Runner was compiled with FIPS mode but BoringSSL is not enabled.")
 }
