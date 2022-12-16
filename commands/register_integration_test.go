7cd1cb733597a0f35c90c30154de821e97160081
Tomasz Maczukin <tomasz@maczukin.pl>
2022-12-16 14:28:23 +0000

    Revert "Merge branch 'ajwalker/upgrade-toml' into 'main'"
    This reverts merge request !3741

--- commands/register_integration_test.go
+++ commands/register_integration_test.go
@@ -615,8 +615,12 @@ shutdown_timeout = 0
   token_expires_at = 0001-01-01T00:00:00Z
   executor = "shell"
   shell = "pwsh"
+  [runners.custom_build_dir]
   [runners.cache]
     MaxUploadedArchiveSize = 0
+    [runners.cache.s3]
+    [runners.cache.gcs]
+    [runners.cache.azure]
 `
 	)
 
@@ -676,8 +680,12 @@ shutdown_timeout = 0
   token_expires_at = 0001-01-01T00:00:00Z
   executor = "shell"
   shell = "pwsh"
+  [runners.custom_build_dir]
   [runners.cache]
     MaxUploadedArchiveSize = 0
+    [runners.cache.s3]
+    [runners.cache.gcs]
+    [runners.cache.azure]
 `,
 		},
 		"incorrect config template merge": {
