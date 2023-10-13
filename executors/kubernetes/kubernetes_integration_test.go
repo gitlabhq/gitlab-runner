f8747e5325a2d34be3658c3c827ac003aef5ca68
Axel von Bertoldi <avonbertoldi@gitlab.com>
2023-10-13 09:49:42 -0600

    Fix a number of syntax errors

--- executors/kubernetes/kubernetes_integration_test.go
+++ executors/kubernetes/kubernetes_integration_test.go
@@ -1207,7 +1207,7 @@ func testBuildsDirVolumeMountEmptyDirFeatureFlag(t *testing.T, featureFlagName s
 				Name:      "repo",
 				MountPath: "/path/to/builds/dir",
 				Medium:    "Memory",
-				SizeLimit: "1G"
+				SizeLimit: "1G",
 			},
 		},
 	}
