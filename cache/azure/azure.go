6affdc952d6605ede4cb051e29a1a306a0134f93
Stan Hu <stanhu@gmail.com>
2023-10-24 09:48:35 -0700

    Update gocloud.dev to v0.34.0
    gocloud.dev v0.27.0 printed out some debug messages for Azure.
    This has since been fixed in v0.28.0:
    https://github.com/google/go-cloud/pull/3197
    
    https://github.com/Azure/azure-sdk-for-go/pull/19010 moved
    many of the SAS functions into a separate package. Update the API
    accordingly.
    
    v0.34.0 is required for Azure to work properly with SAS tokens due to
    https://github.com/google/go-cloud/issues/3283.
    
    Changelog: changed

--- cache/azure/azure.go
+++ cache/azure/azure.go
@@ -8,6 +8,7 @@ import (
 	"time"
 
 	"github.com/Azure/azure-sdk-for-go/sdk/storage/azblob"
+	"github.com/Azure/azure-sdk-for-go/sdk/storage/azblob/sas"
 
 	"gitlab.com/gitlab-org/gitlab-runner/common"
 )
@@ -23,7 +24,7 @@ type signedURLOptions struct {
 }
 
 func presignedURL(name string, o *signedURLOptions) (*url.URL, error) {
-	sas, err := getSASQueryParameters(name, o)
+	sasQueryParams, err := getSASQueryParameters(name, o)
 	if err != nil {
 		return nil, err
 	}
@@ -33,17 +34,17 @@ func presignedURL(name string, o *signedURLOptions) (*url.URL, error) {
 		domain = o.StorageDomain
 	}
 
-	parts := azblob.BlobURLParts{
+	parts := sas.URLParts{
 		Scheme:        "https",
 		Host:          fmt.Sprintf("%s.%s", o.Credentials.AccountName, domain),
 		ContainerName: o.ContainerName,
 		BlobName:      name,
-		SAS:           sas,
+		SAS:           sasQueryParams,
 	}
 
-	u, err := url.Parse(parts.URL())
+	u, err := url.Parse(parts.String())
 	if err != nil {
-		return nil, fmt.Errorf("failed to parse Azure URL '%s': %w", parts.URL(), err)
+		return nil, fmt.Errorf("failed to parse Azure URL '%s': %w", parts.String(), err)
 	}
 	return u, nil
 }
@@ -57,8 +58,8 @@ func getSASToken(name string, o *signedURLOptions) (string, error) {
 	return sas.Encode(), nil
 }
 
-func getSASQueryParameters(name string, o *signedURLOptions) (azblob.SASQueryParameters, error) {
-	empty := azblob.SASQueryParameters{}
+func getSASQueryParameters(name string, o *signedURLOptions) (sas.QueryParameters, error) {
+	empty := sas.QueryParameters{}
 
 	if o.Credentials.AccountName == "" {
 		return empty, errors.New("missing Azure storage account name")
@@ -72,16 +73,16 @@ func getSASQueryParameters(name string, o *signedURLOptions) (azblob.SASQueryPar
 		return empty, fmt.Errorf("creating Azure signature: %w", err)
 	}
 
-	permissions := azblob.AccountSASPermissions{Read: true}
+	permissions := sas.AccountPermissions{Read: true}
 	if o.Method == http.MethodPut {
-		permissions = azblob.AccountSASPermissions{Write: true}
+		permissions = sas.AccountPermissions{Write: true}
 	}
 
 	// Set the desired SAS signature values and sign them with the
 	// shared key credentials to get the SAS query parameters.
 	// See https://docs.microsoft.com/en-us/rest/api/storageservices/create-service-sas
-	serviceSASValues := azblob.BlobSASSignatureValues{
-		Protocol:      azblob.SASProtocolHTTPS, // Users MUST use HTTPS (not HTTP)
+	serviceSASValues := sas.BlobSignatureValues{
+		Protocol:      sas.ProtocolHTTPS, // Users MUST use HTTPS (not HTTP)
 		StartTime:     time.Now().Add(-1 * time.Hour).UTC(),
 		ExpiryTime:    time.Now().Add(o.Timeout).UTC(),
 		Permissions:   permissions.String(),
@@ -89,7 +90,7 @@ func getSASQueryParameters(name string, o *signedURLOptions) (azblob.SASQueryPar
 		BlobName:      name,
 	}
 
-	sas, err := serviceSASValues.NewSASQueryParameters(credential)
+	sas, err := serviceSASValues.SignWithSharedKey(credential)
 	if err != nil {
 		return empty, fmt.Errorf("creating Azure SAS: %w", err)
 	}
