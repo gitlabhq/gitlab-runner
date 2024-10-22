e719c9b9a3a7cc5fdeb30c3b6784b3375a81d814
Romuald Atchadé <ratchade@gitlab.com>
2024-10-22 00:42:14 +0000

    Merge branch 'sh-stop-testing-windows-legacy-process-strategy' into 'main'
    Stop testing UseWindowsLegacyProcessStrategy for KillerTest
    
    See merge request https://gitlab.com/gitlab-org/gitlab-runner/-/merge_requests/5102
    
    Merged-by: Romuald Atchadé <ratchade@gitlab.com>
    Approved-by: Arran Walker <ajwalker@gitlab.com>
    Co-authored-by: Stan Hu <stanhu@gmail.com>

--- helpers/process/killer_windows_integration_test.go
+++ helpers/process/killer_windows_integration_test.go
@@ -5,31 +5,11 @@ package process_test
 // Cases for Windows that are used in `kill_integration_test.go#TestKiller`.
 func testKillerTestCases() map[string]testKillerTestCase {
 	return map[string]testKillerTestCase{
-		"command not terminated": {
-			alreadyStopped:                  false,
-			skipTerminate:                   true,
-			expectedError:                   "",
-			useWindowsLegacyProcessStrategy: true,
-			useWindowsJobObject:             false,
-		},
-		"command terminated": {
-			alreadyStopped:                  false,
-			skipTerminate:                   false,
-			expectedError:                   "exit status 3",
-			useWindowsLegacyProcessStrategy: true,
-			useWindowsJobObject:             false,
-		},
 		"command terminated, disable useWindowsLegacyProcessStrategy": {
 			alreadyStopped:                  false,
 			skipTerminate:                   false,
 			expectedError:                   "exit status 3",
 			useWindowsLegacyProcessStrategy: false,
-			useWindowsJobObject:             false,
-		},
-		"command already stopped": {
-			alreadyStopped:                  true,
-			expectedError:                   "exit status 1",
-			useWindowsLegacyProcessStrategy: true,
 			useWindowsJobObject:             false,
 		},
 		"command terminated via job object": {
