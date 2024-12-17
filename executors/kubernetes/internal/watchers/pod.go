c5de0132c3e171d1c75e6591a877de9cb02ad9c6
Hannes Hörl <hhoerl@gitlab.com>
2024-12-17 12:58:33 +0100

    Implement a pod watcher
    This new pod watcher uses an informer to get updates from the kube api.
    It then does some checks on the pod & containers to figure out, if the
    pod is still healthy / ready to run builds.
    
    It uses a "private" informer factory for that, which it creates on
    Start() and destroys on Stop().

---
+++ executors/kubernetes/internal/watchers/pod.go
@@ -0,0 +1,198 @@
+package watchers
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"slices"
+	"sync/atomic"
+	"time"
+
+	"gitlab.com/gitlab-org/gitlab-runner/common"
+	"gitlab.com/gitlab-org/gitlab-runner/executors/kubernetes/internal/pull"
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/cache"
+)
+
+// emitErrorTimeout is the time we wait for a consumer of the error channel to receive a message. We must not block the
+// informer, and if there is nobody listening for an error, there is no need to block on that anyway. We still give
+// consumers some time, it was observed that _sometimes_ it takes a bit for the channel receiver to actually be able to
+// receive (looking at you, windows).
+const emitErrorTimeout = time.Millisecond
+
+type PodWatcher struct {
+	factory *selfManagedInformerFactory
+
+	podName atomic.Value
+
+	// Don't send on this channel in a blocking manner, so that we don't block the informer. You can use the emitError()
+	// method.
+	errors chan error
+}
+
+// NewPodWatcher creates a pod watcher based on the kubeclient, namespace, and labels.
+// Internally, it creates a informer factory which can manage itself, so that it can be used and shut down properly.
+func NewPodWatcher(ctx context.Context, kubeClient kubernetes.Interface, namespace string, labels map[string]string) *PodWatcher {
+	return &PodWatcher{
+		factory: newScopedInformerFactory(ctx, kubeClient, namespace, labels),
+		errors:  make(chan error),
+	}
+}
+
+// Start starts the watcher, by creating an informer via the informer factory, starting that, waiting for events to
+// come in, and forwarding (terminal) pod errors to the subscriber.
+func (p *PodWatcher) Start() error {
+	gvr := v1.SchemeGroupVersion.WithResource("pods")
+
+	informer, err := p.factory.ForResource(gvr)
+	if err != nil {
+		return fmt.Errorf("creating informer for pods: %w", err)
+	}
+
+	_, err = informer.Informer().AddEventHandler(p.resourceHandler())
+	if err != nil {
+		return fmt.Errorf("registering event handler: %w", err)
+	}
+
+	p.factory.Start()
+
+	for informer, isSynced := range p.factory.WaitForCacheSync() {
+		if isSynced {
+			continue
+		}
+		err = errors.Join(err, fmt.Errorf("not synced: %s", informer))
+	}
+
+	return err
+}
+
+// UpdatePodName sets the pod name we are interested in
+func (p *PodWatcher) UpdatePodName(podName string) {
+	p.podName.Store(podName)
+}
+
+// Stop shuts down the pod watcher by shutting down its dependants: the informer factory and thus the
+// informers created based on it.
+func (p *PodWatcher) Stop() {
+	p.factory.Shutdown()
+}
+
+// Errors reports observed errors on the pod in question. This method MUST only ever called by one consumer at a time.
+func (p *PodWatcher) Errors() <-chan error {
+	return p.errors
+}
+
+func (p *PodWatcher) onPodChange(pod *v1.Pod) {
+	err := checkTerminalPodErrors(pod)
+	if err != nil {
+		_ = p.emitError(err)
+	}
+}
+
+// emitError sends out an error in a non-blocking way, so that the informer is not blocked.
+func (p *PodWatcher) emitError(err error) error {
+	select {
+	case p.errors <- err:
+		return nil
+	case <-time.After(emitErrorTimeout):
+		return fmt.Errorf("timed out")
+	}
+}
+
+func (p *PodWatcher) resourceHandler() cache.ResourceEventHandler {
+	return cache.FilteringResourceEventHandler{
+		FilterFunc: func(obj any) bool {
+			// We need to filter on the pod name; when the executor retries on pull issues, it starts the machinery from
+			// fresh. While this is happening, the old pod might still be terminating. We don't care about these old pods
+			// anymore in this context, and thus don't want to receive updates thereof.
+			return asPod(obj).GetName() == p.currentPodName()
+		},
+		Handler: cache.ResourceEventHandlerFuncs{
+			AddFunc: func(obj any) {
+				p.onPodChange(asPod(obj))
+			},
+			UpdateFunc: func(_, newObj any) {
+				p.onPodChange(asPod(newObj))
+			},
+			DeleteFunc: func(obj any) {
+				pod := asPod(obj)
+				err := fmt.Errorf("pod %q is deleted", pod.GetNamespace()+"/"+pod.GetName())
+				_ = p.emitError(err)
+			},
+		},
+	}
+}
+
+func (p *PodWatcher) currentPodName() string {
+	if podName, ok := p.podName.Load().(string); ok {
+		return podName
+	}
+	return ""
+}
+
+// asPod is a convenience helper to type-assert an untyped object to a pod.
+// If the object is not a pod, we plain ignore it.
+// This is mostly to make gocritic happy, by "catching" the assertion error.
+func asPod(obj any) *v1.Pod {
+	pod, _ := obj.(*v1.Pod)
+	return pod
+}
+
+// checkTerminalPodErrors checks a pod for errors that are terminal, ie. the system can't recover from.
+func checkTerminalPodErrors(pod *v1.Pod) error {
+	fullPodName := fmt.Sprintf("%s/%s", pod.GetNamespace(), pod.GetName())
+
+	dt := getPodCondition(pod, v1.DisruptionTarget)
+	if dt != nil && dt.Status == v1.ConditionTrue {
+		return fmt.Errorf("pod %q is disrupted: reason %q, message %q", fullPodName, dt.Reason, dt.Message)
+	}
+
+	if pod.DeletionTimestamp != nil {
+		return fmt.Errorf("pod %q is being deleted", fullPodName)
+	}
+
+	// TODO: check ephemeral containers?
+	allContainerStatuses := slices.Concat(pod.Status.InitContainerStatuses, pod.Status.ContainerStatuses)
+	if err := checkTerminalContainerErrors(allContainerStatuses); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// checkTerminalContainerErrors checks indiviual container statuses for errors we can't recover from.
+func checkTerminalContainerErrors(containerStatuses []v1.ContainerStatus) error {
+	// TODO: dedup with ./executors/kubernetes/util.go
+	for _, containerStatus := range containerStatuses {
+		if containerStatus.Ready {
+			continue
+		}
+
+		waiting := containerStatus.State.Waiting
+		if waiting == nil {
+			continue
+		}
+
+		switch waiting.Reason {
+		case "InvalidImageName":
+			return &common.BuildError{Inner: fmt.Errorf("image pull failed: %s", waiting.Message)}
+		case "ErrImagePull", "ImagePullBackOff":
+			msg := fmt.Sprintf("image pull failed: %s", waiting.Message)
+			imagePullErr := &pull.ImagePullError{Message: msg, Container: containerStatus.Name, Image: containerStatus.Image}
+			return &common.BuildError{Inner: imagePullErr, FailureReason: common.ImagePullFailure}
+		}
+	}
+
+	return nil
+}
+
+func getPodCondition(pod *v1.Pod, condition v1.PodConditionType) *v1.PodCondition {
+	conditions := pod.Status.Conditions
+	for i := range conditions {
+		if conditions[i].Type == condition {
+			return &conditions[i]
+		}
+	}
+	return nil
+}
---
+++ executors/kubernetes/internal/watchers/pod_test.go
@@ -0,0 +1,167 @@
+//go:build !integration
+
+package watchers
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes/fake"
+)
+
+var (
+	defaultName      = "some-pod"
+	defaultNamespace = "some-namespace"
+	defaultLabels    = map[string]string{"some": "label"}
+)
+
+func TestPodWatcher(t *testing.T) {
+	tests := map[string]struct {
+		pod            *v1.Pod
+		shouldDelete   bool
+		expectedErrMsg string
+	}{
+		"deleted": {
+			pod:            defaultPod(),
+			shouldDelete:   true,
+			expectedErrMsg: "is deleted",
+		},
+		"deletion timestamp": {
+			pod:            withDeletionTimestamp(defaultPod()),
+			expectedErrMsg: "is being deleted",
+		},
+		"disruption": {
+			pod:            withDisruption(defaultPod(), "disruption-msg", "disruption-reason"),
+			expectedErrMsg: `disrupted: reason "disruption-reason", message "disruption-msg"`,
+		},
+		"invalid image": {
+			pod:            withContainerWaiting(defaultPod(), "some-container", "some-msg", "InvalidImageName"),
+			expectedErrMsg: "image pull failed: some-msg",
+		},
+		"pull error": {
+			pod:            withContainerWaiting(defaultPod(), "some-container", "some-msg", "ErrImagePull"),
+			expectedErrMsg: "image pull failed: some-msg",
+		},
+		"pull backoff": {
+			pod:            withContainerWaiting(defaultPod(), "some-container", "some-msg", "ImagePullBackOff"),
+			expectedErrMsg: "image pull failed: some-msg",
+		},
+		"healthy pod": {
+			pod: defaultPod(),
+		},
+		"error, but different pod name": {
+			pod: withName(withDeletionTimestamp(defaultPod()), "some-other-pod"),
+		},
+		"error, but different namespace": {
+			pod: withNameSpace(withDeletionTimestamp(defaultPod()), "some-other-namespace"),
+		},
+		// Note: the fake client infrastructure does not handle labels correctly, thus this unit test would fail.
+		// The real client, informer, ... however handle that correctly
+		// "error, but different labels": {
+		// 	pod: withLabels(withDeletionTimestamp(defaultPod()), map[string]string{"some": "other", "random": "labels"}),
+		// },
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			ctx, cancel := context.WithCancel(context.Background())
+			defer cancel()
+			fakeKubeClient := fake.NewSimpleClientset()
+
+			podWatcher := NewPodWatcher(ctx, fakeKubeClient, defaultNamespace, defaultLabels)
+
+			err := podWatcher.Start()
+			assert.NoError(t, err, "starting pod watcher")
+
+			factory := podWatcher.factory
+
+			_, err = fakeKubeClient.CoreV1().Pods(test.pod.GetNamespace()).Create(ctx, test.pod, metav1.CreateOptions{})
+			assert.NoError(t, err, "creating pod")
+
+			if test.shouldDelete {
+				err := fakeKubeClient.CoreV1().Pods(test.pod.GetNamespace()).Delete(ctx, test.pod.GetName(), metav1.DeleteOptions{})
+				assert.NoError(t, err, "deleting pod")
+			}
+
+			podWatcher.UpdatePodName(defaultName)
+
+			err = waitForError(podWatcher.Errors())
+			if test.expectedErrMsg == "" {
+				assert.NoError(t, err, "expected not to receive an error from the pod watcher")
+			} else {
+				assert.ErrorContains(t, err, test.expectedErrMsg, "expected a error like %q from the pod watcher", test.expectedErrMsg)
+			}
+
+			podWatcher.Stop()
+			// We check the factory's context to see if it had actually been canceled and by that inferring that it has been shut
+			// down.
+			assert.Equal(t, context.Canceled, factory.ctx.Err(), "expected factory's context to be canceled")
+		})
+	}
+}
+
+func waitForError(ch <-chan error) error {
+	to := time.After(10 * time.Millisecond)
+	select {
+	case <-to:
+		return nil
+	case err := <-ch:
+		return err
+	}
+}
+
+func defaultPod() *v1.Pod {
+	pod := &v1.Pod{}
+	pod.SetName(defaultName)
+	pod.SetNamespace(defaultNamespace)
+	pod.SetLabels(defaultLabels)
+	return pod
+}
+
+func withName(pod *v1.Pod, name string) *v1.Pod {
+	pod.SetName(name)
+	return pod
+}
+
+func withNameSpace(pod *v1.Pod, namespace string) *v1.Pod {
+	pod.SetNamespace(namespace)
+	return pod
+}
+
+func withLabels(pod *v1.Pod, labels map[string]string) *v1.Pod {
+	pod.SetLabels(labels)
+	return pod
+}
+
+func withDeletionTimestamp(pod *v1.Pod) *v1.Pod {
+	now := metav1.Now()
+	pod.DeletionTimestamp = &now
+	return pod
+}
+
+func withDisruption(pod *v1.Pod, msg, reason string) *v1.Pod {
+	pod.Status.Conditions = append(pod.Status.Conditions, v1.PodCondition{
+		Status:  v1.ConditionTrue,
+		Message: msg,
+		Reason:  reason,
+		Type:    v1.DisruptionTarget,
+	})
+	return pod
+}
+
+func withContainerWaiting(pod *v1.Pod, containerName, msg, reason string) *v1.Pod {
+	pod.Status.ContainerStatuses = append(pod.Status.ContainerStatuses, v1.ContainerStatus{
+		Name: containerName,
+		State: v1.ContainerState{
+			Waiting: &v1.ContainerStateWaiting{
+				Reason:  reason,
+				Message: msg,
+			},
+		},
+	})
+	return pod
+}
