49c2ec17ecc26c9054bebc22b158950e384e99c6
Anthony Juckel <ajuckel@gmail.com>
2025-01-11 14:06:23 -0600

    Remove explicit buildx builder creation for local images

--- Makefile.runner_helper.mk
+++ Makefile.runner_helper.mk
@@ -50,8 +50,6 @@ helper-image-host: export HOST_ARCH ?= $(shell go env GOARCH)
 helper-image-host: export HOST_FLAVOR ?= alpine-3.21
 helper-image-host: export RUNNER_IMAGES_VERSION ?= $(shell grep "RUNNER_IMAGES_VERSION:" .gitlab/ci/_common.gitlab-ci.yml | awk -F': ' '{ print $$2 }' | tr -d '"')
 helper-image-host: helper-bin-linux
-	docker buildx create --name builder --use --driver docker-container $(shell docker context show) || true
-	mkdir -p out/helper-images
 	cd dockerfiles/runner-helper && docker buildx bake --progress plain host-image
 
 # Make sure the fips target is first since it's less general
