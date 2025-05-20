8b5314f74d9fa526c8dd5cce028ec8dfaf186da2
Lin Jen-Shin <jen-shin@gitlab.com>
2025-05-20 17:24:59 +0800

    Handle the new glrtr- prefix

--- helpers/shorten_token.go
+++ helpers/shorten_token.go
@@ -9,13 +9,18 @@ func ShortenToken(token string) string {
 	case len(token) < 8:
 		return token
 	case token[:5] == "glrt-":
-		// Token is prefixed with CREATED_RUNNER_TOKEN_PREFIX: glrt- (for Gitlab Runner Token).
+		// Token is prefixed with CREATED_RUNNER_TOKEN_PREFIX: glrt- (for GitLab Runner Token).
 		// Let's add some more characters in order to compensate.
 		return token[5:14]
+	case token[:6] == "glrtr-":
+		// Token is prefixed with REGISTRATION_RUNNER_TOKEN_PREFIX: glrtr- (for GitLab Runner Token).
+		// This prefix will only be used for runners registered via the registration token.
+		// Let's add some more characters in order to compensate.
+		return token[6:15]
 	case token[:6] == "glcbt-":
 		return token[6:15]
 	case token[:2] == "GR" && len(token) >= 17 && strings.IndexFunc(token[2:9], isInvalidPrefixRune) == -1:
-		// Token is prefixed with RUNNERS_TOKEN_PREFIX: GR (for Gitlab Runner) combined with the rotation
+		// Token is prefixed with RUNNERS_TOKEN_PREFIX: GR (for GitLab Runner) combined with the rotation
 		// date decimal-to-hex-encoded. Let's add some more characters in order to compensate.
 		return token[:17]
 	default:
