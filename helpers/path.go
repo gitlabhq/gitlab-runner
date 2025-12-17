52c0137f5b96c44fffee3cd485194fa7a105fac2
Vishal Tak <vtak@gitlab.com>
2025-12-17 10:00:00 +0530

    Cleanup dangling virtualbox resources
    When job is cancelled while the VM is being created,
    the VM creation is aborted midway and it leaves dangling
    resources which are not cleaned up. This prevents from a new job
    getting scheduled in certain cases since the VM name matches.
    
    To resolve this, we delete all the hard drives associated to the
    VM and delete the machine folder where the VM data is present.

--- helpers/path.go
+++ helpers/path.go
@@ -1,6 +1,9 @@
 package helpers
 
-import "strings"
+import (
+	"path/filepath"
+	"strings"
+)
 
 func ToBackslash(path string) string {
 	return strings.ReplaceAll(path, "/", "\\")
@@ -8,4 +11,12 @@ func ToBackslash(path string) string {
 
 func ToSlash(path string) string {
 	return strings.ReplaceAll(path, "\\", "/")
+}
+
+// IsImmediateChild checks if child is an immediate subdirectory of parent.
+// Both paths are cleaned and converted to absolute paths before comparison.
+// If it is not able to determine the relative path between parent and child, returns false.
+func IsImmediateChild(parent, child string) bool {
+	rel, err := filepath.Rel(filepath.Clean(parent), filepath.Clean(child))
+	return err == nil && rel != "." && !strings.HasPrefix(rel, "..") && !strings.Contains(rel, string(filepath.Separator))
 }
--- helpers/virtualbox/control_test.go
+++ helpers/virtualbox/control_test.go
@@ -51,3 +51,50 @@ func TestCurrentSnapshotNameRegex(t *testing.T) {
 		assert.NotEqual(t, test.expectedSnapshotName, actual[1])
 	}
 }
+
+func Test_extractHDDInfo(t *testing.T) {
+	var tests = []struct {
+		name              string
+		output            string
+		expectedUUIDs     []string
+		expectedLocations []string
+	}{
+		{
+			name:              "0 HDDs",
+			output:            "",
+			expectedUUIDs:     []string{},
+			expectedLocations: []string{},
+		},
+		{
+			name:              "1 HDD",
+			output:            "UUID:           a8b5aa23-2110-435a-9bdd-8f28e4b840a7\nParent UUID:    base\nLocation:       /home/directory/VirtualBox VMs/vm 1/ vm 1.vdi\nStorage format: VDI\n",
+			expectedUUIDs:     []string{"a8b5aa23-2110-435a-9bdd-8f28e4b840a7"},
+			expectedLocations: []string{"/home/directory/VirtualBox VMs/vm 1/ vm 1.vdi"},
+		},
+		{
+			name:              "1 HDD with windows style \\r\\n new lines",
+			output:            "UUID:           a8b5aa23-2110-435a-9bdd-8f28e4b840a7\r\nParent UUID:    base\r\nLocation:       /home/directory/VirtualBox VMs/vm 1/ vm 1.vdi\r\nStorage format: VDI\r\n",
+			expectedUUIDs:     []string{"a8b5aa23-2110-435a-9bdd-8f28e4b840a7"},
+			expectedLocations: []string{"/home/directory/VirtualBox VMs/vm 1/ vm 1.vdi"},
+		},
+		{
+			name:              "2 matching HDDs for VM",
+			output:            "UUID:           a8b5aa23-2110-435a-9bdd-8f28e4b840a7\nParent UUID:    base\nLocation:       /home/directory/VirtualBox VMs/vm 1/ vm 1.vdi\nStorage format: VDI\n\nUUID:           97bcddfc-b184-4d26-b197-fe287a77fe55\nParent UUID:    base\nLocation:       /home/directory/VirtualBox VMs/vm 2/ vm 2.vdi\nStorage format: VDI\n",
+			expectedUUIDs:     []string{"a8b5aa23-2110-435a-9bdd-8f28e4b840a7", "97bcddfc-b184-4d26-b197-fe287a77fe55"},
+			expectedLocations: []string{"/home/directory/VirtualBox VMs/vm 1/ vm 1.vdi", "/home/directory/VirtualBox VMs/vm 2/ vm 2.vdi"},
+		},
+	}
+	for _, test := range tests {
+		// fixture validation
+		assert.Equal(t, len(test.expectedUUIDs), len(test.expectedLocations))
+
+		actualHDDs := extractHDDInfo(test.output)
+
+		assert.Equal(t, len(test.expectedUUIDs), len(actualHDDs))
+
+		for i, hdd := range actualHDDs {
+			assert.Equal(t, test.expectedUUIDs[i], hdd[1])
+			assert.Equal(t, test.expectedLocations[i], hdd[2])
+		}
+	}
+}
