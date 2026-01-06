4d10dce98f9e2976334537b9f9a528fcede6768c
Axel von Bertoldi <avonbertoldi@gitlab.com>
2026-01-06 13:09:34 -0700

    mage target to create pulp config

--- magefiles/pulp.go
+++ magefiles/pulp.go
@@ -4,13 +4,22 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"strings"
 
 	"github.com/magefile/mage/mg"
+	"github.com/magefile/mage/sh"
+
+	"gitlab.com/gitlab-org/gitlab-runner/magefiles/mageutils"
 	"gitlab.com/gitlab-org/gitlab-runner/magefiles/pulp"
 )
 
 type Pulp mg.Namespace
+
+var (
+	pulpURL  = mageutils.EnvOrDefault("PULP_URL", "https://pulp.gitlab.com/")
+	username = mageutils.EnvOrDefault("PULP_USER", "runner")
+)
 
 // SupportedOSVersions prints the list of OS/versions for which runner packages will be released (for the given package type and release branch)
 func (p Pulp) SupportedOSVersions(dist, branch string) error {
@@ -20,4 +29,21 @@ func (p Pulp) SupportedOSVersions(dist, branch string) error {
 	}
 	fmt.Println(strings.Join(os, "\n"))
 	return nil
+}
+
+// CreateConfig creates a working pulp configuration for pulp.gitlab.com
+func (p Pulp) CreateConfig() error {
+	password, ok := os.LookupEnv("PULP_PASSWORD")
+	if !ok || strings.TrimSpace(password) == "" {
+		return fmt.Errorf("missing or invalid PULP_PASSWORD")
+	}
+
+	return sh.RunV("pulp", "config", "create", "--overwrite",
+		"--base-url", pulpURL,
+		"--api-root", "/pulp/",
+		"--verify-ssl",
+		"--format", "json",
+		"--username", username,
+		"--password", password,
+	)
 }
