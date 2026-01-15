2baba4eb537c8e483a1f7fe3c19aea8f5decffc1
Axel von Bertoldi <avonbertoldi@gitlab.com>
2026-01-15 15:58:07 -0700

    Add rpmPusher type
    rpmPusher implements pushing rpm packages. This is much more involved
    than deb packages. For starters, there are separate repositories for
    every distro/release/arch tuple. Secondly, the way the
    gitlab-runner-helper-images package is handles is very different.
    Because its arch is `all`, it has to be uploaded to each
    distro/release/arch tuple. This sucks Because that package is huge
    (~500MB), and even though pulp will dedup it on the server, we still
    have to upload it.
    
    An alternative way to do this is to upload the package once to one
    distro/release/arch repo, and then "link" that uploaded file to all the
    other relevant distro/release/arch repos.
    
    In fact, this approach is more efficient than we we do for deb packages
    or in packageclound because we only upload each package once to any one
    distro/release/arch repo, then link it to the other repos, which is a
    cheap operation.
    
    Even though handling the other rpm packages the same way as the deb
    packages technically works, I've used this approach for all rpm
    packages, both for the efficiency and to minimize code.

--- magefiles/pulp/push.go
+++ magefiles/pulp/push.go
@@ -28,6 +28,10 @@ type (
 	debPusher struct {
 		basePusher
 	}
+	rpmPusher struct {
+		basePusher
+		archs []string
+	}
 )
 
 func (p *basePusher) runPulpCmd(args ...string) error {
@@ -75,4 +79,179 @@ func (p *debPusher) pushArgs(release, pkg string) []string {
 		"--component", "main",
 		"--repository", pulpRepo,
 	}
+}
+
+const (
+	helperImagePkg = "gitlab-runner-helper-images"
+)
+
+// For rpm packages, the pulp repo is configured such that:
+// * The arch will NOT be auto-detected; it is encoded in the pulp repo name.
+// * There's one repo per distribution/release/arch tuple, e.g. fedora-43-x86_64
+// * Multiple repos can point to and expose the same package/file. This means...
+// * We can upload a package/file once, (to any repo), then link it to all the other relevant repos.
+// * All packages/files are handled this way, including the gitlab-runner-helper-images package.
+// * The only difference is that this same file is linked to repos for all archs.
+func (p *rpmPusher) Push(releases, pkgFiles []string) error {
+	slog.Debug("Will push the following packages to pulp", "packages", pkgFiles, "releases", releases)
+	pool := pool.New().WithMaxGoroutines(p.concurrency).WithErrors()
+	for _, pkgFile := range pkgFiles {
+		pool.Go(func() error {
+			pkgInfo, err := p.getRPMInfo(pkgFile)
+			if err != nil {
+				return fmt.Errorf("failed to get rpm package info from file %s: %w", pkgFile, err)
+			}
+			// for runner packages we only specify one arch, corresponding to the package's arch.
+			archs := []string{pkgInfo.arch}
+			if pkgInfo.name == helperImagePkg {
+				// for the helper images package we specify all the archs.
+				archs = p.archs
+			}
+			return p.pushPackage(pkgFile, pkgInfo, releases, archs)
+		})
+	}
+
+	return pool.Wait()
+}
+
+// Even though `i686` is the correct arch label for rpm packages, pulp wants `i386`.
+// See https://gitlab.com/gitlab-org/gitlab-runner/-/merge_requests/6038#note_2992748581
+func (p *rpmPusher) normalizeArch(arch string) string {
+	if arch == "i686" {
+		return "i386"
+	}
+	return arch
+}
+
+func (p *rpmPusher) pulpRepo(release, arch string) string {
+	return "runner-" + p.branch + "-" + strings.ReplaceAll(release, "/", "-") + "-" + arch
+}
+
+func (p *rpmPusher) pushArgs(pkgFile, repo string) []string {
+	return []string{"rpm", "content", "upload", "--file", pkgFile, "--repository", repo}
+}
+
+func (p *rpmPusher) linkArgs(repo, href string) []string {
+	return []string{"rpm", "repository", "content", "modify", "--repository", repo, "--add-content", `[{"pulp_href": "` + href + `"}]`}
+}
+
+// Push the specific package file to all the specified releases and architectures
+func (p *rpmPusher) pushPackage(pkgFile string, pkgInfo rpmInfo, releases, archs []string) error {
+	archs = lo.Map(archs, func(a string, _ int) string {
+		return p.normalizeArch(a)
+	})
+
+	// push the package to the fist release/arch
+	repo := p.pulpRepo(releases[0], archs[0])
+	slog.Debug("Pushing", "package", pkgFile, "release", releases[0], "arch", archs[0], "version", pkgInfo.version)
+	href, err := p.doPush(pkgFile, repo)
+	if err != nil {
+		return fmt.Errorf("failed to push file %s to repo %s: %w", pkgFile, repo, err)
+	}
+
+	slog.Debug("Package successfully uploaded", "file", pkgFile, "pulp_href", href)
+
+	// link the package to all other relevant releases/archs
+	for _, release := range releases {
+		for _, arch := range archs {
+			slog.Debug("Linking", "package", pkgFile, "release", release, "arch", arch, "version", pkgInfo.version)
+			repo := p.pulpRepo(release, arch)
+			err = errors.Join(err, p.runPulpCmd(p.linkArgs(repo, href)...))
+		}
+	}
+	return err
+}
+
+type repoPushResult struct {
+	PulpHref string `json:"pulp_href,omitempty"`
+}
+
+func (p *rpmPusher) doPush(pkgFile, repo string) (string, error) {
+	args := p.pushArgs(pkgFile, repo)
+
+	out := bytes.Buffer{}
+	if err := p.execCmd(&out, "pulp", args...); err != nil {
+		return "", err
+	}
+
+	result := repoPushResult{}
+	err := json.NewDecoder(&out).Decode(&result)
+	if err != nil {
+		return "", fmt.Errorf("failed to unmarshal response: %w", err)
+	}
+
+	if result.PulpHref == "" {
+		return "", fmt.Errorf("package upload response had empty 'pulp_href'")
+	}
+	return result.PulpHref, nil
+}
+
+// getRPMInfo runs rpm -qi on the given filename and extracts the Version, Name and Architecture fields
+func (p *rpmPusher) getRPMInfo(filename string) (rpmInfo, error) {
+	out := bytes.Buffer{}
+	if err := p.execCmd(&out, "rpm", "-qi", filename); err != nil {
+		return rpmInfo{}, fmt.Errorf("failed to query rpm package %q: %w", filename, err)
+	}
+
+	// Parse the output to extract Version field
+	info, err := parseRPMInfo(&out)
+	if err != nil {
+		return rpmInfo{}, fmt.Errorf("failed to parse version from rpm output: %w", err)
+	}
+
+	return info, nil
+}
+
+var (
+	versionRE = regexp.MustCompile(`Version\s*:\s*([^ ]+)\s*`)
+	archRE    = regexp.MustCompile(`Architecture\s*:\s*([^ ]+)\s*`)
+	nameRE    = regexp.MustCompile(`Name\s*:\s*([^ ]+)\s*`)
+)
+
+type rpmInfo struct {
+	name    string
+	version string
+	arch    string
+}
+
+func (i *rpmInfo) parseLine(line string) bool {
+	if matches := versionRE.FindStringSubmatch(line); len(matches) == 2 {
+		i.version = matches[1]
+	} else if matches := nameRE.FindStringSubmatch(line); len(matches) == 2 {
+		i.name = matches[1]
+	} else if matches := archRE.FindStringSubmatch(line); len(matches) == 2 {
+		i.arch = matches[1]
+	}
+
+	return i.allFieldsFound()
+}
+
+func (i *rpmInfo) allFieldsFound() bool {
+	return i.name != "" && i.arch != "" && i.version != ""
+}
+
+// parseRPMInfo extracts the Version field from rpm -qi output.
+// The output is not structured, but contains lines like:
+// "Version      : <version>"
+// "Architecture : <arch>"
+// "Name         : <name>"
+func parseRPMInfo(out io.Reader) (rpmInfo, error) {
+	scanner := bufio.NewScanner(out)
+	info := rpmInfo{}
+
+	for scanner.Scan() {
+		if info.parseLine(scanner.Text()) {
+			break
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		return rpmInfo{}, fmt.Errorf("error reading rpm output: %w", err)
+	}
+
+	if !info.allFieldsFound() {
+		return rpmInfo{}, fmt.Errorf("at least one field not found in rpm output")
+	}
+
+	return info, nil
 }
