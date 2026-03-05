03138262d03027845b039b5b31a54257c6a78164
Vishal Tak <vtak@gitlab.com>
2026-03-05 11:49:55 +0530

    Update github.com/santhosh-tekuri/jsonschema to v6

--- commands/internal/configfile/validation.go
+++ commands/internal/configfile/validation.go
@@ -1,10 +1,11 @@
 package configfile
 
 import (
+	"bytes"
 	"encoding/json"
 
 	jsonschema_generator "github.com/invopop/jsonschema"
-	jsonschema_validator "github.com/santhosh-tekuri/jsonschema/v5"
+	jsonschema_validator "github.com/santhosh-tekuri/jsonschema/v6"
 	"github.com/sirupsen/logrus"
 	"gitlab.com/gitlab-org/gitlab-runner/common"
 )
@@ -27,7 +28,15 @@ func init() {
 	if err != nil {
 		panic(err)
 	}
-	configSchema = jsonschema_validator.MustCompileString("config_schema.json", string(schema))
+	doc, err := jsonschema_validator.UnmarshalJSON(bytes.NewReader(schema))
+	if err != nil {
+		panic(err)
+	}
+	c := jsonschema_validator.NewCompiler()
+	if err := c.AddResource("config_schema.json", doc); err != nil {
+		panic(err)
+	}
+	configSchema = c.MustCompile("config_schema.json")
 }
 
 func validate(config *common.Config) error {
@@ -38,13 +47,12 @@ func validate(config *common.Config) error {
 		}
 	}()
 
-	// Validation must be done on generic types so we re-unmarshal the config into an interface{}
+	// Validation must be done on generic types so we re-unmarshal the config into a JSON value
 	configString, err := json.Marshal(config)
 	if err != nil {
 		panic(err)
 	}
-	var jsonValue interface{}
-	err = json.Unmarshal(configString, &jsonValue)
+	jsonValue, err := jsonschema_validator.UnmarshalJSON(bytes.NewReader(configString))
 	if err != nil {
 		panic(err)
 	}
